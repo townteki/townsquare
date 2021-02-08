bd341c9a9683cca5a1d633c3d6edcf60f5eb438f
Milan M2 <mmeldo@gmail.com>
2021-02-08 23:54:12 +0100

    Shootout redraw prompt
    New card select prompt for shootout redraw.
    Fix for the onceConditional event emit function.

--- server/game/gamesteps/shootout/drawhandprompt.js
+++ server/game/gamesteps/shootout/drawhandprompt.js
@@ -8,9 +8,10 @@ class DrawHandPrompt extends UiPrompt {
         this.drawCounts = drawCounts;
         if (this.drawCounts == null) {
             this.drawCounts = this.players.map(player => {
-                return { player: player, number: 5, handDrawn: false };
+                return { player: player, number: 5, handDrawn: false, handRedrawn: false };
             });            
-        }       
+        }
+        this.selectedCards = [];
         this.players.forEach(player => this.updateDrawCount(player));
     }
 
@@ -33,6 +34,14 @@ class DrawHandPrompt extends UiPrompt {
                     { text: ' - Cards', arg: 'lessCards' }
                 ]
             };
+        } else if (!this.getDrawCount(player).handRedrawn) {
+            return {
+                menuTitle: 'Select up to ' + this.getDrawCount(player).redraw + ' cards to redraw',
+                buttons: [
+                    { arg: 'redraw', text: 'Done' }
+                ],
+                selectCard: true
+            };
         } else {
             return {
                 menuTitle: 'Reveal draw hand?',
@@ -54,7 +63,10 @@ class DrawHandPrompt extends UiPrompt {
     continue() {
         if (this.game.currentPhase === 'gambling') {
             // in gambling phase hands are drawn in separate step before this prompt
-            this.drawCounts.forEach(drawCount => drawCount.handDrawn = true);
+            this.drawCounts.forEach(drawCount => {
+                drawCount.handDrawn = true;
+                drawCount.handRedrawn = true;
+            });
         }
 
         return super.continue();
@@ -66,11 +78,39 @@ class DrawHandPrompt extends UiPrompt {
         drawCount.number = drawCount.number < 5 ? 5 : drawCount.number;        
     }
 
+    onCardClicked(player, card) {
+        if(card.location !== 'draw hand') {
+            return false;
+        }
+
+        if(this.selectedCards.length >= this.getDrawCount(player).redraw && !this.selectedCards.includes(card)) {
+            return false;
+        }
+
+        if(!this.selectedCards.includes(card)) {
+            this.selectedCards.push(card);
+        } else {
+            this.selectedCards = this.selectedCards.filter(selectedCard => selectedCard !== card);
+        }
+        player.setSelectedCards(this.selectedCards);
+
+    }
+
+    highlightSelectableCards(player) {
+        player.selectCard = true;
+        player.setSelectableCards(player.drawHand);
+    }
+
     onMenuCommand(player, arg) {
         if (arg === 'draw') {
             let drawCount = this.getDrawCount(player);
             player.drawCardsToHand(drawCount.number, 'draw hand');
             this.getDrawCount(player).handDrawn = true;
+            if (this.getDrawCount(player).redraw === 0) {
+                this.getDrawCount(player).handRedrawn = true;
+            } else {
+                this.highlightSelectableCards(player);
+            }
             return false;
         }
         if (arg === 'moreCards') {
@@ -79,6 +119,16 @@ class DrawHandPrompt extends UiPrompt {
         }
         if (arg === 'lessCards') {
             this.updateDrawCount(player, -1);
+            return false;
+        }
+        if (arg === 'redraw') {
+            let numberToRedraw = this.selectedCards.length;
+            player.discardCards(this.selectedCards);
+            player.drawCardsToHand(numberToRedraw, 'draw hand');
+            this.getDrawCount(player).handRedrawn = true;
+            this.selectedCards = [];
+            player.clearSelectedCards();
+            player.clearSelectableCards();
             return false;
         }
         if (arg === 'revealdraw') {
