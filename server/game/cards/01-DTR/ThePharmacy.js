c2c37107fca90be147f532036e72636b08f72749
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-03-11 07:57:05 -0800

    Implemented The Pharmacy

---
+++ server/game/cards/01-DTR/ThePharmacy.js
@@ -0,0 +1,25 @@
+const DeedCard = require('../../deedcard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class ThePharmacy extends DeedCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'The Pharmacy',
+            playType: ['noon'],
+            cost: ability.costs.bootSelf(),
+			target: {
+                activePromptTitle: 'Select dude to unboot',
+                cardCondition: { location: 'play area', booted: true },
+                cardType: ['dude']
+            },
+            handler: context => {
+				this.game.resolveGameAction(GameActions.unbootCard({ card: context.target }), context);
+                this.game.addMessage('{0} uses {1} to unboot {2}', context.player, this, context.target);
+            }
+        });
+    }
+}
+
+ThePharmacy.code = '01078';
+
+module.exports = ThePharmacy;
