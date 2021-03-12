f87d4518382c660808bc217766b5968964fa5a18
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-03-12 14:28:17 -0800

    Implemented Dr. Dawn Edwards, and fixed two small typos in Eve Henry's comments

--- server/game/cards/01-DTR/DrDawnEdwards.js
+++ server/game/cards/01-DTR/DrDawnEdwards.js
@@ -1,11 +1,11 @@
 const DudeCard = require('../../dudecard.js');
 const GameActions = require('../../GameActions/index.js');
 
-// For Eve Henry, we cannot use traitReaction because it is in effect only if card is in play. Since the triggering
+// For Dr. Dawn Edwards, we cannot use traitReaction because it is in effect only if card is in play. Since the triggering
 // event is on discarded, Eve at that time is not in play therefore her trigger will not work.
 // Instead we use one time conditional game wide event trigger for her that is put in place when she enters play and
 // removed once she leaves. `leavesPlay` function executes after the discard event so we are safe here.
-class EveHenry extends DudeCard {
+class DrDawnEdwards extends DudeCard {
     entersPlay() {
         super.entersPlay();
         this.game.onceConditional('onCardDiscarded', { condition: event => event.card === this && event.isCasualty }, 
@@ -19,22 +19,22 @@ class EveHenry extends DudeCard {
     }
 
     eventHandler(event) {
-        // Dr. Dawn Edwards code is 01052
-        if(!this.controller.cardsInPlay.find(card => card.code === '01052')) {
+        // Eve Henry's code is 01054
+        if(!this.controller.cardsInPlay.find(card => card.code === '01054')) {
             this.game.promptForYesNo(this.controller, {
-                title: 'Do you want to search your deck for Dr. Dawn Edwards?',
+                title: 'Do you want to search your deck for Eve Henry?',
                 onYes: player => {
                     this.game.resolveGameAction(
                         GameActions.search({
-                            title: 'Select Dr. Dawn Edwards if you want to put her into play',
-                            match: { type: 'dude', condition: card => card.code === '01052' },
+                            title: 'Select Eve Henry if you want to put her into play',
+                            match: { type: 'dude', condition: card => card.code === '01054' },
                             location: ['draw deck'],
                             numToSelect: 1,
                             message: {
                                 format: '{player} searches their draw deck, and put {searchTarget} into play thanks to {source}'
                             },
                             cancelMessage: {
-                                format: '{player} searches their draw deck thanks to {source}, but does not find Dr. Dawn Edwards'
+                                format: '{player} searches their draw deck thanks to {source}, but does not find Eve Henry'
                             },
                             handler: card => {
                                 this.game.resolveGameAction(GameActions.putIntoPlay({ 
@@ -56,6 +56,6 @@ class EveHenry extends DudeCard {
     }
 }
 
-EveHenry.code = '01054';
+DrDawnEdwards.code = '01052';
 
-module.exports = EveHenry;
+module.exports = DrDawnEdwards;
