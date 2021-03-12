b6243fa70506d580d6327352ce6696aa8c466a51
AaronSchnabel <aaron.schnabel@gmail.com>
2021-03-12 14:04:20 -0800

    Implemented Reserves (#100)

---
+++ server/game/cards/01-DTR/Reserves.js
@@ -0,0 +1,19 @@
+const ActionCard = require('../../actioncard.js');
+
+class Reserves extends ActionCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Reserves',
+            playType: 'noon',      
+            message: context => 
+                this.game.addMessage('{0} uses {1} to gain a ghost rock', context.player, this),
+            handler: context => {
+                context.player.modifyGhostRock(1);
+            }
+        });
+    }
+}
+
+Reserves.code = '01127';
+
+module.exports = Reserves;
