06f692fb48d9fab8448d416d072ab31bf37d2209
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-03-13 09:54:12 -0800

    Implemented Cheatin' Varmint

---
+++ server/game/cards/01-DTR/CheatinVarmint.js
@@ -0,0 +1,31 @@
+const ActionCard = require('../../actioncard.js');
+
+class CheatinVarmint extends ActionCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Resolution: Lower hand by 2 ranks',
+            playType: ['resolution'],
+            choosePlayer: true,
+            cost: ability.costs.payGhostRock(5),
+            message: context =>
+                this.game.addMessage('{0} pays 5 ghost rock and uses {1} to lower {2}\'s draw hand by 2 ranks. Current rank is {3}', 
+                    context.player, this, context.chosenPlayer, context.chosenPlayer.getTotalRank()),
+            handler: context =>
+                context.chosenPlayer.modifyRank(-2)
+        });
+        this.action({
+            title: 'Cheatin\' Resolution: Lower hand by 2 ranks',
+            playType: ['cheatin resolution'],
+            choosePlayer: true,
+            message: context =>
+                this.game.addMessage('{0} uses {1} to lower {2}\'s draw hand by 2 ranks. Current rank is {3}', 
+                    context.player, this, context.chosenPlayer, context.chosenPlayer.getTotalRank()),
+            handler: context =>
+                context.chosenPlayer.modifyRank(-2)   
+        });
+    }
+}
+
+CheatinVarmint.code = '01129';
+
+module.exports = CheatinVarmint;
