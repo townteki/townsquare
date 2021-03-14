5006171658e230eb7006f4b975a094971070dc6d
AaronSchnabel <aaron.schnabel@gmail.com>
2021-03-14 09:12:28 -0700

    Update server/game/cards/01-DTR/CheatinVarmint.js
    Co-authored-by: Milan M2 <mmeldo@gmail.com>

--- server/game/cards/01-DTR/CheatinVarmint.js
+++ server/game/cards/01-DTR/CheatinVarmint.js
@@ -8,7 +8,7 @@ class CheatinVarmint extends ActionCard {
             choosePlayer: true,
             cost: ability.costs.payGhostRock(5),
             message: context =>
-                this.game.addMessage('{0} pays 5 ghost rock and uses {1} to lower {2}\'s draw hand by 2 ranks. Current rank is {3}', 
+                this.game.addMessage('{0} uses {1} and pays 5 ghost rock to lower {2}\'s draw hand by 2 ranks; Current rank is {3}', 
                     context.player, this, context.chosenPlayer, context.chosenPlayer.getTotalRank()),
             handler: context =>
                 context.chosenPlayer.modifyRank(-2)
