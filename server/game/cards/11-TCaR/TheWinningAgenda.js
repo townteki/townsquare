df303578add310a17b1347522b5c9944e56c7e5f
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-03-23 15:00:53 -0700

    Everything works except changing the prompt when selecting cards to discard

--- server/game/cards/11-TCaR/TheWinningAgenda.js
+++ server/game/cards/11-TCaR/TheWinningAgenda.js
@@ -1,7 +1,27 @@
 const ActionCard = require('../../actioncard.js');
+//const GameActions = require('../../GameActions/index.js');
 
 class TheWinningAgenda extends ActionCard {
     setupCardAbilities() {
+        this.action({
+            title: 'Noon: The Winning Agenda',
+            playType: 'noon',
+            target : {
+                activePromptTitle: 'Select a dude',
+                waitingPromptTitle: 'Waiting for opponent to select a dude',
+                cardCondition: { location: 'play area', condition: card => card.influence <= this.controller.hand.length },
+                cardType: 'dude'
+            },
+            handler: context => {
+                let discardAmount = context.target.influence;
+                if(discardAmount > 3) {
+                    discardAmount = 3;
+                }
+                context.player.discardFromHand(discardAmount, () => true, { title: 'Discard Cards Test Message' });
+                context.player.drawCardsToHand(discardAmount).thenExecute(() => { this.game.addMessage('{0} uses {1} and {2}\'s influence to discard {3} cards and then draw {3} cards', 
+                    context.player, this, context.target, discardAmount)});
+            }
+        });
         this.action({
             title: 'Cheatin\' Resolution: The Winning Agenda',
             playType: 'cheatin resolution',
@@ -11,8 +31,8 @@ class TheWinningAgenda extends ActionCard {
             },
             handler: context => {
                 let rankMod = context.target.influence;
-                if (rankMod > 3) {
-                    rankMod = 3
+                if(rankMod > 3) {
+                    rankMod = 3;
                 }
                 context.player.modifyRank(rankMod);
                 this.game.addMessage('{0} uses {1} to increase their hand rank by {2}; Current rank is {3}', 
