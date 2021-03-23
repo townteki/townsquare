1e4e2fc4f30017cdc038130d7d23ec7422e6ae91
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-03-22 19:20:08 -0700

    Implemented Point Blank, got cost correct finally

--- server/game/cards/01-DTR/PointBlank.js
+++ server/game/cards/01-DTR/PointBlank.js
@@ -1,33 +1,32 @@
 const ActionCard = require('../../actioncard.js');
 const GameActions = require('../../GameActions/index.js');
 
-//var bulletcount;
-
 class PointBlank extends ActionCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Resolution: Point Blank',
-            playType: ['resolution'],
+            playType: 'resolution',
             choosePlayer: false,
             cost: [
-                ability.costs.boot({
-                    controller: 'current',
-                    cardCondition: { location: 'play area', participating: true, condition: card => {
-                        card.iStud() } },
-                    cardType: ['dude'],
-                })],
-            target: {
-                choosingPlayer: 'opponent',
-                activePromptTitle: 'Select your dude to ace',
-                waitingPromptTitle: 'Waiting on opponent to select a dude',
-                cardCondition: { location: 'play area', condition: card => card.bullets < context.costs.boot.bullets, participating: true },
-                cardType: ['dude']
-            },
-            message: context => {
-                this.game.addMessage('{0} uses {1} to boot {2}, which causes {3} to ace {4}', context.player, this, context.costs.boot, context.player.getOpponent(), context.target);
-            },
+                ability.costs.boot(card =>
+                    card.getType() === 'dude' &&
+                    card.isStud() && 
+                    card.isParticipating()
+                )
+            ],
             handler: context => {
-                this.game.resolveGameAction(GameActions.aceCard({ card: context.target }), context);
+                this.game.promptForSelect(context.player.getOpponent(), {
+                    activePromptTitle: 'Select a dude to ace',
+                    waitingPromptTitle: 'Waiting for opponent to select dude',
+                    cardCondition: { location: 'play area', controller: context.player.getOpponent(), participating: true, condition: card => card.bullets < context.costs.boot.bullets },
+                    cardType: 'dude',
+                    onSelect: (opponent, cardToAce) => {
+                        this.game.resolveGameAction(GameActions.aceCard({card: cardToAce}, context));
+                        this.game.addMessage('{0} uses {1} to boot {2}, forcing {3} to ace {4}',
+                            context.player, this, context.costs.boot, opponent, cardToAce);
+                        return true;
+                    }
+                });
             }
         });
     }
