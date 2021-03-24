911cbb306b527ffd4ad229eccd0a616f821462eb
Ola Faxe <olafaxe@gmail.com>
2021-03-24 15:38:39 +0100

    removed spaces and changec owner to context.player

--- server/game/cards/01-DTR/RecruitmentDrive.js
+++ server/game/cards/01-DTR/RecruitmentDrive.js
@@ -9,21 +9,21 @@ class RecruitmentDrive extends ActionCard {
             playType: 'noon',
             target: 'townsquare',
             message: context =>
-                this.game.addMessage('{0} plays {1} marking {2}.', context.player, this, context.target),
-            onSuccess: (context) => {
+                this.game.addMessage('{0} plays {1} marking {2}', context.player, this, context.target),
+            onSuccess: (job, context) => {
                 this.game.resolveGameAction(
                     GameActions.search(
                         {
                             title: 'Select a dude or deed to put into play',
                             location: 'discard pile',
                             match: { condition: card => card.getType() === 'dude' || card.getType() === 'deed' },
-                            player: context.leader.owner,
+                            player: context.player,
                             numToSelect: 1,
-                            cancelMessage:(context) =>
-                                this.game.addMessage('{0} searches their discard pile and finds nothing', context.leader.owner, this),
-                            handler: (card) => {
-                                this.game.resolveStandardAbility(StandardActions.putIntoPlayWithReduction(5), context.leader.owner, card);
-                                this.game.addMessage('{0} searches their discard pile and finds {1}.', context.leader.owner, card);
+                            cancelMessage:() =>
+                                this.game.addMessage('{0} searches their discard pile and finds nothing', context.player, this),
+                            handler: card => {
+                                this.game.resolveStandardAbility(StandardActions.putIntoPlayWithReduction(5), context.player, card);
+                                this.game.addMessage('{0} searches their discard pile and finds {1}', context.player, card);
                             }
                         }), context
                 );                
