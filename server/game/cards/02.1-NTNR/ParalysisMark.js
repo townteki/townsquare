8d1cb2a4bd6dfd7c4f309b8565b8bdddecca9230
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-04-01 07:17:30 -0700

    Fixed the missing semicolon

--- server/game/cards/02.1-NTNR/ParalysisMark.js
+++ server/game/cards/02.1-NTNR/ParalysisMark.js
@@ -16,7 +16,7 @@ class ParalysisMark extends SpellCard {
             difficulty: context => context.target.value,
             onSuccess: (context) => {
                 this.game.resolveGameAction(GameActions.bootCard({ card: context.target }), context);
-                this.game.addMessage('{0} uses {1} to paralyze {2}', context.player, this, context.target)
+                this.game.addMessage('{0} uses {1} to paralyze {2}', context.player, this, context.target);
             },
             source: this
         });
