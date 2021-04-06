eee45564416a816d02d1c92dd78c93a90438017f
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-04-06 15:24:20 -0700

    Initial commit

---
+++ server/game/cards/04.1-FJ/Confession.js
@@ -0,0 +1,65 @@
+const GameActions = require('../../GameActions/index.js');
+const SpellCard = require('../../spellcard.js');
+
+class Confession extends SpellCard {
+    setupCardAbilities(ability) {
+        this.spellAction({
+            title: 'Noon: Miracle 6',
+            playType: 'noon',
+            cost: ability.costs.bootSelf(),
+            difficulty: 6,
+            target: {
+                activePromptTitle: 'Select a dude to add bounty to',
+                waitingPromptTitle: 'Waiting for opponent to select a dude',
+                cardCondition: { 
+                    location: 'play area', 
+                    condition: card =>
+                        card.gamelocation === this.gamelocation || 
+                        card.getGameLocation().isAdjacent(this.gamelocation)
+                },
+                cardType: ['dude'],
+                gameAction: 'addBounty'
+            },
+            onSuccess: (context) => {
+                this.game.resolveGameAction(GameActions.bootCard({ card: this.parent }), context);
+                this.game.resolveGameAction(GameActions.addBounty({
+                    card: context.target, amount: this.parent.getSkillRating('blessed')
+                }));
+                this.game.addMessage('{0} uses {1} to boot {2} and add bounty to {3}', context.player, this, this.parent, context.target);
+            },
+            source: this
+        });
+
+        this.spellAction({
+            title: 'Noon: Miracle 7',
+            playType: 'noon',
+            cost: ability.costs.bootSelf(),
+            difficulty: 6,
+            target: {
+                activePromptTitle: 'Select a dude to take bounty from',
+                waitingPromptTitle: 'Waiting for opponent to select a dude',
+                cardCondition: { 
+                    location: 'play area', 
+                    condition: card =>
+                        (card.gamelocation === this.gamelocation || 
+                        card.getGameLocation().isAdjacent(this.gamelocation)) &&
+                        card.isWanted();
+                },
+                cardType: ['dude'],
+                gameAction: 'addBounty'
+            },
+            onSuccess: (context) => {
+                this.game.resolveGameAction(GameActions.bootCard({ card: this.parent }), context);
+                this.game.resolveGameAction(GameActions.addBounty({
+                    card: context.target, amount: this.parent.getSkillRating('blessed')
+                }));
+                this.game.addMessage('{0} uses {1} to boot {2} and add bounty to {3}', context.player, this, this.parent, context.target);
+            },
+            source: this
+        });
+    }
+}
+
+Confession.code = '06017';
+
+module.exports = Confession;
