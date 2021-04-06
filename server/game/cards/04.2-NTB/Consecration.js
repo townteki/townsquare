971d8ac3d596010ff538edcff22ee637460ad673
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-04-06 14:34:00 -0700

    Added comment

--- server/game/cards/04.2-NTB/Consecration.js
+++ server/game/cards/04.2-NTB/Consecration.js
@@ -23,6 +23,7 @@ class Consecration extends SpellCard {
                     ]
                 }));
                 this.game.addMessage('{0} uses {1} to increase {3}\'s bullets and influence by 2 and make them a stud', context.player, this, context.target);
+                /* Check if this is a shootout and if so, reduce casualties */
                 if(this.game.shootout) {
                     context.player.modifyCasualties(-3);
                     this.game.addMessage('{0} also uses {1} to reduce their casualties by 3 this round', context.player, this);
