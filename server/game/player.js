3baaacac10871cb133b71e73838394894a7c0353
Milan M2 <mmeldo@gmail.com>
2021-04-07 00:01:34 +0200

    Card: Many Speak as One, Sun-Touched Raven

--- server/game/player.js
+++ server/game/player.js
@@ -1341,7 +1341,7 @@ class Player extends Spectator {
         }
 
         dude.moveToLocation(destination.uuid);
-        if(!options.isCardEffect) {
+        if(!options.isCardEffect && !dude.isToken()) {
             this.game.addMessage(moveMessage, this, dude, destination.locationCard);
         }
     }
@@ -1369,7 +1369,8 @@ class Player extends Spectator {
 
         var params = {
             player: this,
-            card: card
+            card: card,
+            targetLocation
         };
 
         if(card.location === 'play area') {
