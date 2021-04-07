6b47d9b388c2d14d5ed63012debdc1c4bfda10bd
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-04-07 08:32:12 -0700

    added thenExecute to second noon action

--- server/game/cards/04.1-FJ/Confession.js
+++ server/game/cards/04.1-FJ/Confession.js
@@ -54,9 +54,11 @@ class Confession extends SpellCard {
             onSuccess: (context) => {
                 this.game.resolveGameAction(GameActions.removeBounty({
                     card: context.target, amount: 1
-                }));
-                context.player.modifyGhostRock(1);
-                this.game.addMessage('{0} uses {1} to take 1 bounty from {2} and add it to their stash', context.player, this, context.target);
+                })).thenExecute(() => {
+                    context.player.modifyGhostRock(1).thenExecute(() => {
+                        this.game.addMessage('{0} uses {1} to take 1 bounty from {2} and add it to their stash', context.player, this, context.target);
+                    });
+                });
             },
             source: this
         });
