48d0d3846ac87c025c22a281c157388b2f636164
Milan M2 <mmeldo@gmail.com>
2021-04-11 15:52:18 +0200

    Card: Nathan Shane, Sight Beyond Sight (#374)
    Updated GameActions.lookAtHand

--- server/game/GameActions/LookAtHand.js
+++ server/game/GameActions/LookAtHand.js
@@ -9,14 +9,45 @@ class LookAtHand extends GameAction {
         return opponent && opponent.hand.length > 0;
     }
 
-    createEvent({ player, opponent, context }) {
+    createEvent({ player, opponent, title, numToShow, numToSelect, condition, onSelect, onCancel, context }) {
+        let params = {
+            title: title || `Look at ${opponent.name}'s hand`,
+            numToShow: numToShow || opponent.hand.length,
+            numToSelect: numToSelect || 1,
+            condition: condition || (() => true),
+            onSelect: onSelect || (() => true),
+            onCancel: onCancel || (() => true)
+        };
+        if(params.numToShow > opponent.hand.length) {
+            params.numToShow = opponent.hand.length;
+        }
+        let workHand = opponent.hand;
+        const cardsToLook = [];
+        while(cardsToLook.length < params.numToShow && workHand.length > 0) {
+            let selectedCard = workHand[Math.floor(Math.random() * (workHand.length - 1))];
+            workHand = workHand.filter(card => card !== selectedCard);
+            cardsToLook.push(selectedCard);
+        }
         return this.event('onLookAtHand', { player, opponent }, event => {
+            const revealFunc = card => cardsToLook.includes(card);
+            context.game.cardVisibility.addRule(revealFunc);
             context.game.promptForSelect(event.player, {
-                activePromptTitle: `Look at ${event.opponent.name}'s hand`,
+                activePromptTitle: params.title,
                 source: context.source,
-                revealTargets: true,
-                cardCondition: card => card.location === 'hand' && card.controller === event.opponent,
-                onSelect: () => true
+                multiSelect: true,
+                numCards: params.numToSelect,
+                cardCondition: card => card.location === 'hand' && 
+                    card.controller === event.opponent && 
+                    context.game.isCardVisible(card, event.player) &&
+                    params.condition(card),
+                onSelect: (player, cards) => {
+                    params.onSelect(player, cards);
+                    return true;
+                },
+                onCancel: player => params.onCancel(player)
+            });
+            context.game.queueSimpleStep(() => {
+                context.game.cardVisibility.removeRule(revealFunc);
             });
         });
     }
