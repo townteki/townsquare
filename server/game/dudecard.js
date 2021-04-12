e1bffcae1b0a0c9d057ff17a6a80f2d9f3effbc4
Milan M2 <mmeldo@gmail.com>
2021-04-12 19:57:01 +0200

    Update Interrupt to ReactionBefore plus Lay On Hands (#375)
    Plus implementation of Lay On Hands card

--- server/game/dudecard.js
+++ server/game/dudecard.js
@@ -186,7 +186,13 @@ class DudeCard extends DrawCard {
     }
 
     sendHome(options = {}, context) {
-        this.game.resolveGameAction(GameActions.moveDude({ card: this, targetUuid: this.owner.outfit.uuid, options }), context);
+        if(options.needToBoot) {
+            this.game.resolveGameAction(GameActions.bootCard({ card: this }), context);
+        }
+        this.game.resolveGameAction(GameActions.moveDude({ card: this, targetUuid: this.controller.outfit.uuid, options }), context);
+        if(options.fromPosse && this.game.shootout) {
+            this.game.shootout.removeFromPosse(this);
+        } 
     }
 
     moveToLocation(destinationUuid) {
@@ -345,7 +351,7 @@ class DudeCard extends DrawCard {
         if(!shootout) {
             return { canJoin: false };
         } 
-        if(this.getGameLocation().isAdjacent(shootout.gamelocation) && (!this.booted || allowBooted)) {
+        if(this.isAdjacent(shootout.gamelocation) && (!this.booted || allowBooted)) {
             return { canJoin: true, needToBoot: true };
         }
 
@@ -353,7 +359,7 @@ class DudeCard extends DrawCard {
             if(this.gamelocation === shootout.leader.gamelocation) {
                 return { canJoin: true, needToBoot: true };
             } 
-            if(this.getGameLocation().isAdjacent(shootout.leader.gamelocation)) {
+            if(this.isAdjacent(shootout.leader.gamelocation)) {
                 return { canJoin: true, needToBoot: true };
             }         
         }
--- server/game/game.js
+++ server/game/game.js
@@ -21,7 +21,7 @@ const EventWindow = require('./gamesteps/eventwindow.js');
 const AbilityResolver = require('./gamesteps/abilityresolver.js');
 const TraitTriggeredAbilityWindow = require('./gamesteps/TraitTriggeredAbilityWindow.js');
 const TriggeredAbilityWindow = require('./gamesteps/TriggeredAbilityWindow.js');
-const InterruptWindow = require('./gamesteps/InterruptWindow');
+const ReactionBeforeWindow = require('./gamesteps/ReactionBeforeWindow');
 const Event = require('./event.js');
 const NullEvent = require('./NullEvent');
 const AtomicEvent = require('./AtomicEvent.js');
@@ -860,7 +860,7 @@ class Game extends EventEmitter {
             if(context.ability && context.ability.title !== 'Trade') {
                 this.currentPlayWindow.markActionAsTaken(context.player);
             }
-        } else if(this.currentPhase !== 'setup' || this.hasOpenInterruptOrReactionWindow()) {
+        } else if(this.currentPhase !== 'setup' || this.hasOpenReactionWindow()) {
             this.addAlert('danger', '{0} uses {1} outside of a play window', context.player, context.source);
         }
     }
@@ -903,7 +903,7 @@ class Game extends EventEmitter {
         this.queueSimpleStep(() => this.abilityWindowStack.pop());
     }
 
-    openInterruptWindowForAttachedEvents(event) {
+    openReactionBeforeWindowForAttachedEvents(event) {
         let attachedEvents = [];
         for(let concurrentEvent of event.getConcurrentEvents()) {
             attachedEvents = attachedEvents.concat(concurrentEvent.attachedEvents);
@@ -919,7 +919,7 @@ class Game extends EventEmitter {
             groupedEvent.addChildEvent(attachedEvent);
         }
 
-        this.queueStep(new InterruptWindow(this, groupedEvent, () => this.postEventCalculations()));
+        this.queueStep(new ReactionBeforeWindow(this, groupedEvent, () => this.postEventCalculations()));
     }
 
     registerAbility(ability, event) {
@@ -939,7 +939,7 @@ class Game extends EventEmitter {
         }
     }
 
-    hasOpenInterruptOrReactionWindow() {
+    hasOpenReactionWindow() {
         return this.abilityWindowStack.length !== 0;
     }
 
