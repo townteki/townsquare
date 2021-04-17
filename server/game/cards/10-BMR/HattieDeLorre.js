cf898dfdeac496cfdd55b7f1c3576f9654b1d6ce
Milan M2 <mmeldo@gmail.com>
2021-04-17 21:20:10 +0200

    Card: Hattie DeLorre, Zachary Deloria (#397)

--- server/game/cards/10-BMR/HattieDeLorre.js
+++ server/game/cards/10-BMR/HattieDeLorre.js
@@ -1,9 +1,28 @@
-class DummyCard {
-    setupCardAbilities() {
+const DudeCard = require('../../dudecard.js');
 
+class HattieDeLorre extends DudeCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Hattie DeLorre',
+            playType: ['shootout'],
+            target: {
+                activePromptTitle: 'Select a dude',
+                cardCondition: { location: 'play area', controller: 'opponent', participating: true, wanted: true },
+                cardType: ['dude']
+            },
+            message: context => this.game.addMessage('{0} uses {1} to reduce {2}\'s bullets by 5', context.player, this, context.target),
+            handler: context => {
+                this.applyAbilityEffect(context.ability, ability => ({
+                    match: context.target,
+                    effect: [
+                        ability.effects.modifyBullets(-5)
+                    ]
+                }));
+            }
+        });
     }
 }
 
-DummyCard.code = '';
+HattieDeLorre.code = '18016';
 
-module.exports = DummyCard;
+module.exports = HattieDeLorre;
