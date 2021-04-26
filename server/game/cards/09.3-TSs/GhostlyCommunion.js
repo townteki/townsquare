70b831ddd3ce3684dcd8d9d2e843390ba6d32833
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-04-26 11:07:49 -0700

    changed location tet

--- server/game/cards/09.3-TSs/GhostlyCommunion.js
+++ server/game/cards/09.3-TSs/GhostlyCommunion.js
@@ -1,20 +1,20 @@
+const { LeaderPosse } = require('../../Constants/ShootoutStatuses.js');
 const GameActions = require('../../GameActions/index.js');
 const SpellCard = require('../../spellcard.js');
 
 class GhostlyCommunion extends SpellCard {
     setupCardAbilities(ability) {
         this.spellAction({
-            title: 'Noon:',
+            title: 'Shootout:',
             playType: 'shootout:join',
             cost: ability.costs.bootSelf(),
             difficulty: 7,
-            target: {
-                activePromptTitle: 'Select your dude to join',
-                cardCondition: { location: 'play area', participating: false },
-                cardType: ['dude'],
-                gameAction: 'joinPosse'
-            },
+            condition: () => this.game.shootout &&
+                (this.game.shootout.shootoutLocation.hasKeyword('holy ground') || this.game.shootout.shootoutLocation.isAdjacent(this.game.location.hasKeyword('holy ground'))),
             onSuccess: (context) => {
+                this.game.resolveGameAction(GameActions.joinPosse({ card: this.parent }), context);
+                this.parent.setAsStud();
+                this.game.addMessage('{0} uses {1} to move {2} and have them join the posse and make them a stud', context.player, this, this.parent );
             },
             source: this
         });
