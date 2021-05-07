aed6ecf3efc77316fb1bb9914f5e50eb200457f8
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-07 13:39:15 -0700

    Implemented Personal Ornithopter

---
+++ server/game/cards/07.1-DDeeds/PersonalOrnithopter.js
@@ -0,0 +1,37 @@
+const GameActions = require('../../GameActions/index.js');
+const GoodsCard = require('../../goodscard.js');
+
+class PersonalOrnithopter extends GoodsCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Repeat Shootout: Join Posse',
+            playType: 'shootout:join',
+            cost: ability.costs.payGhostRock(1),
+            condition: () => this.game.shootout,
+            actionContext: { card: this.parent, gameAction: 'joinPosse'},
+            repeatable: true,
+            message: context =>
+                this.game.addMessage('{0} uses {1} to have {2} soar into their posse.', context.player, this, this.parent),
+            handler: () => {
+                this.game.resolveGameAction(GameActions.joinPosse({ card: this.parent }));
+            }
+        });
+
+        this.action({
+            title: 'Shootout: Flee',
+            playType: 'shootout',
+            cost: ability.costs.bootSelf(),
+            message: context => this.game.addMessage('{0} uses {1} to send {2} home booted', context.player, this, this.parent),
+            handler: context => {
+                this.game.shootout.sendHome(this.parent, context);
+                if(this.game.shootout.checkEndCondition()) {
+                    this.game.shootout.cancelStep();
+                }
+            }
+        });
+    }
+}
+
+PersonalOrnithopter.code = '11019';
+
+module.exports = PersonalOrnithopter;
