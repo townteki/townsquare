13ca034c2b5888998b02ce7dabf9929fad6167fc
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-08 08:05:08 -0700

    Not sure how to finish this one out

--- server/game/cards/09.3-TSs/GhostlyCommunion.js
+++ server/game/cards/09.3-TSs/GhostlyCommunion.js
@@ -1,16 +1,35 @@
-const { LeaderPosse } = require('../../Constants/ShootoutStatuses.js');
 const GameActions = require('../../GameActions/index.js');
 const SpellCard = require('../../spellcard.js');
 
 class GhostlyCommunion extends SpellCard {
     setupCardAbilities(ability) {
         this.spellAction({
-            title: 'Shootout:',
+            title: 'Noon: Move Dude',
+            playType: 'noon',
+            cost: ability.costs.bootSelf(),
+            difficulty: 5,
+            target: {
+                activePromptTitle: 'Choose a location to move your dude to',
+                waitingPromptTitle: 'Waiting for opponent to choose a location',
+                cardCondition: card => {card.location === 'play area' && (card.hasKeyword('holy ground') || card.adjacentLocations().hasKeyword('holy ground'))},
+                cardType: 'location'
+            },
+            onSuccess: (context) => {
+                this.game.resolveGameAction(GameActions.moveDude({
+                    card: this.parent,
+                    targetUuid: context.target.gamelocation
+                }));
+            },
+            source: this
+        });
+
+        this.spellAction({
+            title: 'Shootout: Join Posse',
             playType: 'shootout:join',
             cost: ability.costs.bootSelf(),
             difficulty: 7,
             condition: () => this.game.shootout &&
-                (this.game.shootout.shootoutLocation.hasKeyword('holy ground') || this.game.shootout.shootoutLocation.isAdjacent(this.game.location.hasKeyword('holy ground'))),
+                (this.game.shootout.shootoutLocation.hasKeyword('holy ground') || this.game.shootout.shootoutLocation.adjacentLocations().hasKeyword('holy ground')),
             onSuccess: (context) => {
                 this.game.resolveGameAction(GameActions.joinPosse({ card: this.parent }), context);
                 this.parent.setAsStud();
