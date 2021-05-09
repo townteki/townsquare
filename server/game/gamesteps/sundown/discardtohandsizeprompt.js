df3e88660d0f47f3b9d3f3124620e35dc44ea0dd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-05-09 09:44:33 -0700

    adds discard-to-hand-size step to sundown phase.

---
+++ server/game/gamesteps/sundown/discardtohandsizeprompt.js
@@ -0,0 +1,53 @@
+const BaseStep = require('../basestep');
+
+class DiscardToHandSizePrompt extends BaseStep {
+    constructor(game) {
+        super(game);
+        this.players = this.game.getPlayersInFirstPlayerOrder();
+    }
+
+    continue() {
+        while(this.players.length) {
+            let currentPlayer = this.players.shift();
+            if(! currentPlayer.isOverHandsizeLimit()) {
+                this.game.addMessage('{0} already meets their hand size limit', currentPlayer);
+            } else {
+                this.promptPlayerToDiscard(currentPlayer);
+                return false;
+            }
+        }
+        this.game.raiseEvent('onHandsizeLimitChecked');
+    }
+
+    promptPlayerToDiscard(currentPlayer) {
+        let overHandsize = currentPlayer.hand.length - currentPlayer.handSize;
+        let cardOrCards = overHandsize === 1 ? 'card' : 'cards';
+        this.game.promptForSelect(currentPlayer, {
+            ordered: true,
+            mode: 'exactly',
+            numCards: overHandsize,
+            activePromptTitle: `Select ${overHandsize} ${cardOrCards} to discard down to hand size (top first)`,
+            waitingPromptTitle: 'Waiting for opponent to discard down to hand size',
+            cardCondition: card => card.location === 'hand' && card.controller === currentPlayer,
+            onSelect: (player, cards) => this.discardCards(player, cards),
+            onCancel: (player) => this.cancelSelection(player)
+        });
+    }
+
+    discardCards(player, cards) {
+        // Reverse the order selection so that the first card selected ends up
+        // on the top of the discard pile.
+        cards = cards.reverse();
+        player.discardCards(cards, false, () => {
+            this.game.addMessage('{0} discards {1} to meet hand size limit', player, cards);
+        });
+        return true;
+    }
+
+    cancelSelection(player) {
+        this.game.addAlert('danger', '{0} continues without discarding down to hand size', player, this);
+        return true;
+    }
+}
+
+module.exports = DiscardToHandSizePrompt;
