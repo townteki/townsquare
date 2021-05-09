5a4f0b65cf33f22115045e4dfa4380870eaa5a13
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2021-05-09 10:39:21 -0700

    Card: B&B Attorneys (#445)

---
+++ server/game/cards/01-DTR/BBAttorneys.js
@@ -0,0 +1,62 @@
+const DeedCard = require('../../deedcard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class BBAttorneys extends DeedCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'B & B Attorneys',
+            playType: ['noon'],
+            cost: ability.costs.bootSelf(),
+            target: {
+                activePromptTitle: 'Select a wanted dude',
+                cardCondition: { location: 'play area', wanted: true },
+                cardType: ['dude']
+            },
+            handler: context => {
+                this.abilityContext = context;
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: 'Raise or lower the bounty by one?',
+                        buttons: [
+                            {
+                                text: 'Raise by one',
+                                method: 'raise'
+                            },
+                            {
+                                text: 'Lower by one',
+                                method: 'lower'
+                            }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    raise(player) {
+        this.applyBountyEffect(player, 1);
+        return true;
+    }
+
+    lower(player) {
+        this.applyBountyEffect(player, -1);
+        return true;
+    }
+
+    applyBountyEffect(player, amount) {
+        let text = 'raise';
+        if(amount < 0) {
+            text = 'lower';
+            this.game.resolveGameAction(GameActions.removeBounty({ card: this.abilityContext.target }), this.abilityContext);
+        } else {
+            this.game.resolveGameAction(GameActions.addBounty({ card: this.abilityContext.target }), this.abilityContext);
+        }
+
+        this.game.addMessage('{0} uses {1} to {2} {3}\'s bounty by one', player, this, text, this.abilityContext.target);
+    }
+}
+
+BBAttorneys.code = '01061';
+
+module.exports = BBAttorneys;
