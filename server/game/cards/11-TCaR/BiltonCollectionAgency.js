22f53b6f2cd8dbedad15b986749d8daf6a10b4e7
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-10 14:07:23 -0700

    Implemented Bilton Collection Agency

--- server/game/cards/11-TCaR/BiltonCollectionAgency.js
+++ server/game/cards/11-TCaR/BiltonCollectionAgency.js
@@ -1,26 +1,23 @@
 const DeedCard = require('../../deedcard.js');
-const GameActions = require('../../GameActions/index.js');
 
 class BiltonCollectionAgency extends DeedCard {
     setupCardAbilities(ability) {
         this.reaction({
             title: 'Collect Extra Production',
             when: {
-                onProductionRecieved: event =>
-                    event.player === this.controllingPlayer
+                onProductionReceived: event =>
+                    event.player === this.player
             },
             cost: ability.costs.bootSelf(),
             target: {
-                activePromptTitle: 'Choose a deed you control',
+                activePromptTitle: 'Choose a deed you control but do not own',
                 waitingPromptTitle: 'Waiting for opponent to choose a deed',
-                cardCondition: {location: 'play area', controller: 'current', condition: card => card.owner !=== this.controllingPlayer},
+                cardCondition: {location: 'play area', controller: 'current', condition: card => card.owner !== this.controllingPlayer},
                 cardType: ['deed']
             },
             handler: context => {
-                this.controller.modifyGhostRock(context.target.production).thenExecute();
-            }
-
-
+                this.controller.modifyGhostRock(context.target.production);
+                this.game.addMessage('{0} uses {1} to gain {2} ghost rock from {3}', this.controller, this, context.target.production, context.target);
             }
         });
     }
