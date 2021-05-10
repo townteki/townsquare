eb73b5dc5e910afed2540977b5b8d1f6777f0e92
Milan M2 <mmeldo@gmail.com>
2021-05-10 20:51:28 +0200

    Card: Emre, The Turkish Bear

--- server/game/cards/06-TLS/EmreTheTurkishBear.js
+++ server/game/cards/06-TLS/EmreTheTurkishBear.js
@@ -1,17 +1,31 @@
 const DudeCard = require('../../dudecard.js');
 
 class EmreTheTurkishBear extends DudeCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.reaction({
             title: 'Emre, The Turkish Bear',
+            triggerBefore: true,
             when: {
-                onCardPulled: event => true
+                onCardPulled: event => event.props.usedBy !== this.uuid &&
+                    event.props.pullingDude && event.props.source &&
+                    event.props.pullingDude.gamelocation === this.gamelocation &&
+                    event.props.source.hasKeyword('hex')
             },
-            cost: [ability.costs.bootSelf()],
-            target: targetselect
-            message: context => this.game.addMessage('{0} uses {1} to ', context.player, this),
             handler: context => {
-                
+                const saveEventHandler = context.event.handler;
+                context.replaceHandler(event => {
+                    const originalCard = event.card;
+                    event.props.usedBy = this.uuid;
+                    event.card = context.player.pull(null, false, event.props);
+                    this.game.addMessage('{0} uses {1} to replace the pulled {2}of{3}({4} ) with a new pull and make him a stud', 
+                        context.player, this, originalCard.value, originalCard.suit, originalCard);
+                    originalCard.owner.handlePulledCard(originalCard);
+                    saveEventHandler(event);
+                });
+                this.untilEndOfRound(ability => ({
+                    match: this,
+                    effect: ability.effects.setAsStud()
+                }));
             }
         });
     }
