066e2f3c30fbf41c1fbeb8ce279949e33621ab24
AaronSchnabel <aaron.schnabel@gmail.com>
2021-05-10 23:51:29 -0700

    Card: Forster Cooke (#453)

---
+++ server/game/cards/07.2-FP/ForsterCooke.js
@@ -0,0 +1,19 @@
+const DudeCard = require('../../dudecard');
+
+class ForsterCooke extends DudeCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetController: 'current',
+            condition: () => this.locationCard.hasOneOfKeywords(['Saloon', 'Casino']),
+            match: card => card.getType() === 'dude' && card !== this,
+            effect: [
+                ability.effects.modifyBullets(1),
+                ability.effects.modifyValue(1)
+            ]
+        });
+    }
+}
+
+ForsterCooke.code = '12002';
+
+module.exports = ForsterCooke;
