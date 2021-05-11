817d71db2dc2f7ba1410e768548d650a03ebaf28
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-11 14:50:39 -0700

    fixed linting

--- server/game/cards/10-BMR/NickelNightInn.js
+++ server/game/cards/10-BMR/NickelNightInn.js
@@ -16,7 +16,7 @@ class NickelNightInn extends DeedCard {
                 gameAction: 'bootDude'
             },
             message: context => {
-                this.game.addMessage('{0} uses {1} to boot {2}', context.player, this, context.target)
+                this.game.addMessage('{0} uses {1} to boot {2}', context.player, this, context.target);
             },
             handler: context => {
                 this.game.resolveGameAction(GameActions.bootCard({ card: context.target }), context);
