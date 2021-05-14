ecc18374ab162391630bf6bcf7906eeb3f12a0b3
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-14 10:56:09 -0700

    Implemented The Mayor's Office

---
+++ server/game/cards/02.3-EDS/TheMayorsOffice.js
@@ -0,0 +1,63 @@
+const DeedCard = require('../../deedcard.js');
+
+class TheMayorsOffice extends DeedCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Noon: Modify Influence',
+            playType: 'noon',
+            cost: ability.costs.bootSelf(),
+            target: {
+                cardCondition: {location: 'play area', condition: card => (card.gamelocation === this.gamelocation || card.isAdjacent(this.gamelocation))},
+                cardType: 'dude',
+                gameAction: ['increaseInfluence', 'decreaseInfluence']
+            },
+            handler: context => {
+                this.abilityContext = context;
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: 'Raise or lower their influence by one?',
+                        buttons: [
+                            {
+                                text: 'Raise by one',
+                                method: 'raise'
+                            },
+                            {
+                                text: 'Lower by one',
+                                method: 'lower'
+                            }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    raise(player) {
+        this.applyInfluenceEffects(player, 1);
+        return true;
+    }
+
+    lower(player) {
+        this.applyInfluenceEffects(player, -1);
+        return true;
+    }
+
+    applyInfluenceEffects(player, amount) {
+        let text = 'raise';
+        if(amount < 0) {
+            text = 'lower';
+        }
+        this.applyAbilityEffect(this.abilityContext.ability, ability => ({
+            match: this.abilityContext.target,
+            effect: [
+                ability.effects.modifyInfluence(amount)
+            ]
+        }));
+        this.game.addMessage('{0} uses {1} to {2} {3}\'s influence by 1', player, this, text, this.abilityContext.target);
+    }
+}
+
+TheMayorsOffice.code = '04012';
+
+module.exports = TheMayorsOffice;
