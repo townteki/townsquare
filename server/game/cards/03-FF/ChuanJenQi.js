23d72b131f6b17329c09b70627d9fa6d93a8ee8b
Milan M2 <mmeldo@gmail.com>
2021-05-14 08:02:59 +0200

    Initial commit

---
+++ server/game/cards/03-FF/ChuanJenQi.js
@@ -0,0 +1,11 @@
+const DudeCard = require('../../dudecard.js');
+
+class ChuanJenQi extends DudeCard {
+    setupCardAbilities(ability) {
+        persistenteffect
+    }
+}
+
+ChuanJenQi.code = '05012';
+
+module.exports = ChuanJenQi;
---
+++ server/game/cards/10-BMR/DiegoLinares.js
@@ -0,0 +1,11 @@
+const DudeCard = require('../../dudecard.js');
+
+class DiegoLinares extends DudeCard {
+    setupCardAbilities(ability) {
+        persistenteffect
+    }
+}
+
+DiegoLinares.code = '18017';
+
+module.exports = DiegoLinares;
