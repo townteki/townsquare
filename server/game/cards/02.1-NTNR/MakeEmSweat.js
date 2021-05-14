784bdd91763a4644c3e145f6bb0ad5e7fe37f468
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-14 12:31:56 -0700

    Implemented Make 'Em Sweat

---
+++ server/game/cards/02.1-NTNR/MakeEmSweat.js
@@ -0,0 +1,35 @@
+const ActionCard = require('../../actioncard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class MakeEmSweat extends ActionCard {
+    setupCardAbilities(ability) {
+        this.action({
+            playType: 'shootout',
+            cost: ability.costs.boot(card =>
+                card.getType() === 'dude' &&
+                card.isParticipating() && card.controller === this.controller &&
+                    !card.booted),
+            target: {
+                cardCondition: {location: 'play area', condition: card => card.isParticipating()},
+                cardType: 'dude'
+            },
+            handler: context => {
+                this.applyAbilityEffect(context.ability, ability => ({
+                    match: context.target,
+                    effect: ability.effects.modifyBullets(-context.costs.boot.bullets)
+                }));
+                if(context.target.bullets <= 0 && !context.target.booted) {
+                    this.game.resolveGameAction(GameActions.bootCard({ card: context.target }), context).thenExecute(() => {
+                        this.game.addMessage('{0} uses {1} to boot {2} and reduce {3}\'s bullets by {4} and boot {3}', context.player, this, context.costs.boot, context.target, context.costs.boot.bullets);
+                    });
+                } else {
+                    this.game.addMessage('{0} uses {1} to boot {2} and reduce {3}\'s bullets by {4}', context.player, this, context.costs.boot, context.target, context.costs.boot.bullets);
+                }
+            }
+        });
+    }
+}
+
+MakeEmSweat.code = '02018';
+
+module.exports = MakeEmSweat;
