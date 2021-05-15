8d8a7cefe5f6a1c28834f919d492e84ca2cd5ce0
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-15 16:50:18 -0700

    Added title and changed ghostrock to GR

--- server/game/cards/02.3-EDS/AngelaPayne.js
+++ server/game/cards/02.3-EDS/AngelaPayne.js
@@ -3,11 +3,12 @@ const DudeCard = require('../../dudecard.js');
 class AngelaPayne extends DudeCard {
     setupCardAbilities(ability) {
         this.action({
+            title: 'Gain 2 Ghost Rock'
             playType: 'noon',
             cost: ability.costs.bootSelf(),
             ifCondition: () => this.locationCard.owner !== this.controller && this.locationCard.getType() === 'deed',
-            ifFailMessage: context => this.game.addMessage('{0} boots {1} but does not gain any ghostrock because she is not at a deed {0} doesn\'t own', context.player, this),
-            message: context => this.game.addMessage('{0} boots {1} to gain 2 ghostrock', context.player, this),
+            ifFailMessage: context => this.game.addMessage('{0} boots {1} but does not gain any GR because she is not at a deed {0} doesn\'t own', context.player, this),
+            message: context => this.game.addMessage('{0} boots {1} to gain 2 GR', context.player, this),
             handler: () => {
                 this.controller.modifyGhostRock(2);
             }
