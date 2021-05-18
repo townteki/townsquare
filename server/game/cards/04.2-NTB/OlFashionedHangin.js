e9ba361c991cec5e62937dadc627949d28957960
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-18 08:58:44 -0700

    Implemented Ol' Fashioned Hangin'

---
+++ server/game/cards/04.2-NTB/OlFashionedHangin.js
@@ -0,0 +1,29 @@
+const ActionCard = require('../../actioncard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class OlFashionedHangin extends ActionCard {
+    setupCardAbilities() {
+        this.job({
+            title: 'Ol\' Fashioned Hangin\'',
+            playType: 'noon',
+            target: {
+                activePromptTitle: 'Choose a wanted dude to hang',
+                cardCondition: { location: 'play area', wanted: true, condition: card => card.bounty >= 2 },
+                cardType: ['dude']
+            },
+            message: context => 
+                this.game.addMessage('{0} uses {1} to lead a job to hang {2}', context.player, this, context.target),
+            onSuccess: (job, context) => {
+                this.game.resolveGameAction(GameActions.aceCard({ card: job.mark }), context);
+                if(job.leader.location === 'play area') {
+                    this.game.addMessage('{0} gains 1 CP', job.leader);
+                    job.leader.modifyControl(1);
+                }
+            }
+        });
+    }
+}
+
+OlFashionedHangin.code = '07020';
+
+module.exports = OlFashionedHangin;
