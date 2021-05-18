df7858686cef4180dd882b2fdebe277f85f215cd
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-05-18 14:16:48 -0700

    Added if statement to check if mark is still in play

--- server/game/cards/04.2-NTB/OlFashionedHangin.js
+++ server/game/cards/04.2-NTB/OlFashionedHangin.js
@@ -14,7 +14,9 @@ class OlFashionedHangin extends ActionCard {
             message: context => 
                 this.game.addMessage('{0} uses {1} to lead a job to hang {2}', context.player, this, context.target),
             onSuccess: (job, context) => {
-                this.game.resolveGameAction(GameActions.aceCard({ card: job.mark }), context);
+                if(job.mark.location === 'play area') {
+                    this.game.resolveGameAction(GameActions.aceCard({ card: job.mark }), context);
+                }
                 if(job.leader.location === 'play area') {
                     this.game.addMessage('{0}\'s hangman, {1}, gains 1 CP as a result of {2}', context.player, job.leader, this);
                     job.leader.modifyControl(1);
