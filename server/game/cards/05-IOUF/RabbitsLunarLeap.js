559dfbbee856f20ce611497321096cdf23fb2bed
Milan M2 <mmeldo@gmail.com>
2021-05-18 08:58:42 +0200

    Card: Rabbit's Lunar Leap

---
+++ server/game/cards/05-IOUF/RabbitsLunarLeap.js
@@ -0,0 +1,11 @@
+const ActionCard = require('../../actioncard.js');
+
+class RabbitsLunarLeap extends ActionCard {
+    setupCardAbilities(ability) {
+        cardaction
+    }
+}
+
+RabbitsLunarLeap.code = '09039';
+
+module.exports = RabbitsLunarLeap;
