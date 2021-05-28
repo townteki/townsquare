f8e71468e71a08d045c4c34869e9e113d20d4850
Milan M2 <mmeldo@gmail.com>
2021-05-28 15:51:17 +0200

    Randall should not give bonus on setup

--- server/game/cards/05-IOUF/Randall.js
+++ server/game/cards/05-IOUF/Randall.js
@@ -4,6 +4,7 @@ class Randall extends DudeCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             condition: () => this.location === 'play area' &&
+                this.game.currentPhase !== 'setup' &&
                 this.getGameLocation().getDudes().length > 2,
             match: this.controller,
             effect: ability.effects.modifyHandSize(1)
