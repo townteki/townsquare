676e892eee2f87ea7affab7010f83cf318229d89
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2021-06-02 12:32:42 -0700

    Card: Epidemic Laboratory (#532)

---
+++ server/game/cards/08-GT/EpidemicLaboratory.js
@@ -0,0 +1,27 @@
+const DeedCard = require('../../deedcard.js');
+
+class EpidemicLaboratory extends DeedCard {
+    setupCardAbilities(ability) {
+        this.job({
+            title: 'Epidemic Laboratory',
+            playType: 'noon',
+            cost: ability.costs.bootSelf(),
+            target: {
+                cardCondition: { 
+                    condition: card => card === this
+                },
+                autoSelect: true
+            },
+            leaderCondition: card => card.isSkilled(),
+            onSuccess: () => {
+                this.game.addMessage('{0} uses {1} to give it +1 GR and +1 CP', this.controller, this);
+                this.modifyProduction(1);
+                this.modifyControl(1);
+            }
+        });
+    }
+}
+
+EpidemicLaboratory.code = '14019';
+
+module.exports = EpidemicLaboratory;
