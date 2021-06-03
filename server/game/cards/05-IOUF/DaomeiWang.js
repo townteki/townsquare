f416f6b33491e31f427b27437790f1c10155bb21
Milan M2 <mmeldo@gmail.com>
2021-06-04 00:10:28 +0200

    Card: Raking Dragons, Daomei Wang (#534)

---
+++ server/game/cards/05-IOUF/DaomeiWang.js
@@ -0,0 +1,41 @@
+const DudeCard = require('../../dudecard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class DaomeiWang extends DudeCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Daomei Wang',
+            when: {
+                onCardEntersPlay: event => event.card === this
+            },
+            target: {
+                activePromptTitle: 'Choose one of your Bandits to move',
+                cardCondition: { 
+                    location: 'play area', 
+                    controller: 'current', 
+                    condition: card => card.gang_code === 'anarchists' && card !== this
+                },
+                cardType: ['dude'],
+                gameAction: 'moveDude'
+            },
+            handler: context => {
+                this.game.promptForLocation(context.player, {
+                    activePromptTitle: 'Select destination',
+                    waitingPromptTitle: 'Waiting for opponent to select location',
+                    onSelect: (player, location) => {
+                        this.game.resolveGameAction(GameActions.moveDude({ 
+                            card: context.target, 
+                            targetUuid: location.uuid
+                        }), context);   
+                        this.game.addMessage('{0} uses {1} to move {2} to {3}.', player, this, context.target, location);                                 
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+}
+
+DaomeiWang.code = '09004';
+
+module.exports = DaomeiWang;
