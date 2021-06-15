f3d0473c0f29497dbf66f00a13778c5490ca6000
AaronSchnabel <aaron.schnabel@gmail.com>
2021-06-15 13:15:59 -0700

    Update server/game/cards/12-2T2D/GuidingWind.js
    Co-authored-by: Milan M2 <mmeldo@gmail.com>

--- server/game/cards/12-2T2D/GuidingWind.js
+++ server/game/cards/12-2T2D/GuidingWind.js
@@ -17,18 +17,12 @@ class GuidingWind extends SpellCard {
                 cardType: 'dude'
             },
             onSuccess: (context) => {
-                if(context.target.influence > 3) {
-                    this.applyAbilityEffect(context.ability, ability => ({
-                        match: context.target,
-                        effect: ability.effects.setBullets(3)
-                    }));
-                    this.game.addMessage('{0} uses {1} to set {2}\'s bullets to 3', context.player, this, context.target);
-                } else {
-                    this.applyAbilityEffect(context.ability, ability => ({
-                        match: context.target,
-                        effect: ability.effects.setBullets(context.target.influence)
-                    }));
-                    this.game.addMessage('{0} uses {1} to set {2}\'s bullets to {3}', context.player, this, context.target, context.target.influence);
+                const newBullets = context.target.influence > 3 ? 3 : context.target.influence;
+                this.applyAbilityEffect(context.ability, ability => ({
+                    match: context.target,
+                    effect: ability.effects.setBullets(newBullets)
+                }));
+                this.game.addMessage('{0} uses {1} to set {2}\'s bullets to {3}', context.player, this, context.target, newBullets);
                 }
             }
         });
