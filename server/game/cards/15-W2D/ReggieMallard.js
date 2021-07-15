899bdbbcb1e294e34939c8caeaef18cad0650745
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-07-15 14:27:56 -0700

    Implemented Reggie Mallard

---
+++ server/game/cards/15-W2D/ReggieMallard.js
@@ -0,0 +1,28 @@
+const DudeCard = require('../../dudecard.js');
+
+class ReggieMallard extends DudeCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            condition: () => this.game.shootout && this.isParticipating(),
+            effect: ability.effects.dynamicBullets(() => this.getHucksterDudesInPosse())
+        });
+    }
+
+    getHucksterDudesInPosse() {
+        if(!this.game.shootout) {
+            return 0;
+        }
+        const Posse = this.game.shootout.getPosseByPlayer(this.controller);
+
+        if(Posse) {
+            // Check if there are more than 4 hucksters in the posse, and if so, return 4, otherwise return the number of hucksters in the posse
+            return (Posse.getDudes(dude => dude.hasKeyword('huckster')).length > 4) ? 4 : Posse.getDudes(dude => dude.hasKeyword('huckster')).length;
+        }
+        return 0;
+    }
+}
+
+ReggieMallard.code = '23024';
+
+module.exports = ReggieMallard;
