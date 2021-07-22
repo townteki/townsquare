2941783f299f0131179be95d1e648144323da7ec
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-07-22 08:39:24 -0700

    Updated to a traitReaction

--- server/game/cards/12-2T2D/PadreErnestodeDiaz.js
+++ server/game/cards/12-2T2D/PadreErnestodeDiaz.js
@@ -1,19 +1,32 @@
 const DudeCard = require('../../dudecard.js');
+const GameActions = require('../../GameActions/index.js');
 
 class PadreErnestodeDiaz extends DudeCard {
-    setupCardAbilities(ability) {
-        this.reaction({
+    setupCardAbilities() {
+        this.traitReaction({
             when: {
                 onDrawHandsRevealed: () => this.controller.getOpponent().isCheatin()
             },
-            cost: ability.costs.boot({
-                type: 'spell',
-                condition: card => card.isMiracle() && card.parent === this
-            }),
-            message: context =>
-                this.game.addMessage('{0} uses {1} to boot {2} and draw a card', context.player, this, context.costs.boot),
             handler: context => {
-                context.player.drawCardsToHand(1, context);
+                context.game.promptForYesNo(context.player, {
+                    title: 'Do you want to use Padre Ernesto de Diaz\'s ability?',
+                    onYes: () => {
+                        this.game.promptForSelect(this.controller, {
+                            promptTitle: this.title,
+                            activePromptTitle: 'Select a miracle to boot',
+                            waitingPromptTitle: 'Waiting for opponent to select a card',
+                            cardCondition: card => card.parent === this && card.getType() === 'spell' && card.isMiracle(),
+                            onSelect: (p, card) => {
+                                this.game.resolveGameAction(GameActions.bootCard({ card: card }, context)).thenExecute(() => {
+                                    this.game.resolveGameAction(GameActions.drawCards({ player: p, amount: 1 }), context).thenExecute(() => {
+                                        this.game.addMessage('{0} uses {1} to boot {2} and draw a card', p, this, card);
+                                    });
+                                });
+                                return true;
+                            }
+                        });            
+                    }
+                });
             }
         });
     }
