7bc1e6c878ad9a3cbea3bb4234493846bafd8bec
Aaron Schnabel <aaron.schnabel@gmail.com>
2021-07-27 08:18:44 -0700

    Implemented You Had ONE Job!

--- server/game/cards/11-TCaR/YouHadOneJob.js
+++ server/game/cards/11-TCaR/YouHadOneJob.js
@@ -1,26 +1,74 @@
 const ActionCard = require('../../actioncard.js');
 
 class YouHadOneJob extends ActionCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
-            title: 'Shootout',
+            title: 'Shootout: Make Stud',
             target: {
-                cardCondition: card => card.location === 'play area' && card.isWanted() && card.bounty >= 3
+                cardCondition: {
+                    location: 'play area',
+                    condition: card => card.isWanted() && card.bounty >= 3
+                },
                 cardType: 'dude'
             },
+            message: context => {
+                this.game.addMessage('{0} uses {1} to make {2} a stud', context.player, this, context.target);
+            },
             handler: context => {
-                this.game.this.game.promptForSelect(player, {
-                    activePromptTitle: 'Select a card',
-                    waitingPromptTitle: 'Waiting for opponent to select card',
-                    cardCondition: card => true,
-                    cardType: 'dude',
-                    onSelect: (player, card) => {
-                        
-                        return true;
-                    }
-                });
-            }
-            }
+                this.applyAbilityEffect(context.ability, ability => ({
+                    match: context.target,
+                    effect: ability.effects.setAsStud()
+                }));
+            },
+            source: this
+        });
+
+        this.action({
+            title: 'Shootout: Make Draw',
+            target: {
+                cardCondition: {
+                    location: 'play area',
+                    condition: card => card.isWanted() && card.bounty >= 3
+                },
+                cardType: 'dude'
+            },
+            message: context => {
+                this.game.addMessage('{0} uses {1} to make {2} a draw', context.player, this, context.target);
+            },
+            handler: context => {
+                this.applyAbilityEffect(context.ability, ability => ({
+                    match: context.target,
+                    effect: ability.effects.setAsDraw()
+                }));
+            },
+            source: this
+        });
+
+        this.action({
+            title: 'Resolution: Reduce Casualties',
+            playType: 'resolution',
+            handler: context => {
+                if(!context.player.isCheatin()) {
+                    this.game.promptForYesNo(context.player, {
+                        title: 'Reduce your casualties by 2?',
+                        onYes: player => {
+                            player.modifyCasualties(-2);
+                            this.game.addMessage('{0} uses {1} to reduce their casualties by 2', player, this);
+                        }
+                    });
+                }
+
+                if(!context.player.getOpponent().isCheatin()) {
+                    this.game.promptForYesNo(context.player.getOpponent(), {
+                        title: 'Reduce your casualties by 2?',
+                        onYes: player => {
+                            player.modifyCasualties(-2);
+                            this.game.addMessage('{0} uses {1} to reduce their casualties by 2', player, this);
+                        }
+                    });
+                }
+            },
+            source: this
         });
     }
 }
