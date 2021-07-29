edae3a7428b4f6226142cddad07216df2a0f3c3f
Milan M2 <mmeldo@gmail.com>
2021-07-30 00:23:28 +0200

    Card: Cochise County Courthouse, Office of Ancestral Affairs

--- server/game/gamesteps/shootout.js
+++ server/game/gamesteps/shootout.js
@@ -32,6 +32,7 @@ class Shootout extends Phase {
 
         this.loserCasualtiesMod = 0;
         this.jobSuccessful = null;
+        this.jobUnopposed = false;
         this.winningPlayer = null;
         this.headlineUsed = false;
         this.shootoutLoseWinOrder = [];
@@ -67,6 +68,7 @@ class Shootout extends Phase {
                         this.queueStep(new ShootoutPossePrompt(this.game, this, this.opposingPlayer));                    
                     },
                     onNo: () => {
+                        this.jobUnopposed = true;
                         this.endShootout();
                     }
                 }));
