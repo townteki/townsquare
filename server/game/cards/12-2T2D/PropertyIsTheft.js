f8284d524a210e1e462f4ff271c2db37d5f895c9
Milan M2 <mmeldo@gmail.com>
2021-08-26 09:39:09 +0200

    Card: Spiritual Society, Property Is Theft (#827)

---
+++ server/game/cards/12-2T2D/PropertyIsTheft.js
@@ -0,0 +1,51 @@
+const OutfitCard = require('../../outfitcard.js');
+
+class PropertyIsTheft extends OutfitCard {
+    setupCardAbilities(ability) {
+        this.traitReaction({
+            when: {
+                onDrawHandsRevealed: event => event.shootout && this.controller.getOpponent().isCheatin()
+            },
+            handler: context => {
+                const opponent = this.controller.getOpponent();
+                if(opponent.getSpendableGhostRock() >= 1) {
+                    if(context.player.isCheatin()) {
+                        opponent.spendGhostRock(1);
+                        this.game.addMessage('{0} pays 1 GR to bank due to {1}', opponent, this);
+                    } else {
+                        this.game.transferGhostRock({
+                            from: context.player,
+                            to: opponent,
+                            amount: 1
+                        });
+                        this.game.addMessage('{0} pays 1 GR to {1} due to {2}', opponent, context.player, this);
+                    }
+                }
+            }
+        });
+
+        this.reaction({
+            title: 'React: Property Is Theft',
+            when: {
+                onGhostRockTransferred: event => event.source === this.owner && 
+                    event.target === this.owner.getOpponent() &&
+                    event.amount > 0
+            },
+            cost: ability.costs.bootSelf(),
+            handler: context => {
+                const amountOfCards = this.game.shootout ? 2 : 1;
+                context.player.drawCardsToHand(amountOfCards, context);
+                context.player.discardFromHand(amountOfCards, discardedCards => 
+                    this.game.addMessage('{0} uses {1} to draw {2} cards and discard {3}', context.player, this, amountOfCards, discardedCards), 
+                {
+                    title: this.title,
+                    activePromptTitle: `Discard up to ${amountOfCards} card(s)`
+                }, context);
+            }
+        });
+    }
+}
+
+PropertyIsTheft.code = '20002';
+
+module.exports = PropertyIsTheft;
