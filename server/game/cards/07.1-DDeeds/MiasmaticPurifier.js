a5e4b4134155bd2d18d49eafb4fe2e691c298dac
Milan M2 <mmeldo@gmail.com>
2021-09-02 08:18:48 +0200

    Card: Miasmatic Purifier, Wagner Memorial Ranch

---
+++ server/game/cards/07.1-DDeeds/MiasmaticPurifier.js
@@ -0,0 +1,11 @@
+const DeedCard = require('../../deedcard.js');
+
+class MiasmaticPurifier extends DeedCard {
+    setupCardAbilities(ability) {
+        //traitreaction
+    }
+}
+
+MiasmaticPurifier.code = '11015';
+
+module.exports = MiasmaticPurifier;
---
+++ server/game/cards/08-GT/WagnerMemorialRanch.js
@@ -0,0 +1,11 @@
+const DeedCard = require('../../deedcard.js');
+
+class WagnerMemorialRanch extends DeedCard {
+    setupCardAbilities(ability) {
+        //cardaction
+    }
+}
+
+WagnerMemorialRanch.code = '14015';
+
+module.exports = WagnerMemorialRanch;
