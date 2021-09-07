1882571abad73aff9e8edf0aa4248130eabf693e
Milan M2 <mmeldo@gmail.com>
2021-09-07 09:43:28 +0200

    Card: Yasmin Tian, Look What I Found!

---
+++ server/game/cards/15-W2D/LookWhatIFound.js
@@ -0,0 +1,11 @@
+const ActionCard = require('../../actioncard.js');
+
+class LookWhatIFound extends ActionCard {
+    setupCardAbilities(ability) {
+        cardaction
+    }
+}
+
+LookWhatIFound.code = '23048';
+
+module.exports = LookWhatIFound;
