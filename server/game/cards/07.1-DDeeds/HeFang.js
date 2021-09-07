5927328f8e9a27ca7e2678af1517b9521fc39467
Milan M2 <mmeldo@gmail.com>
2021-09-07 09:35:34 +0200

    Card: He Fang, Nicholas Kramer

---
+++ server/game/cards/07.1-DDeeds/HeFang.js
@@ -0,0 +1,35 @@
+const DudeCard = require('../../dudecard.js');
+const StandardActions = require('../../PlayActions/StandardActions.js');
+
+class HeFang extends DudeCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Noon: He Fang',
+            playType: ['noon'],
+            cost: ability.costs.bootSelf(),
+            target: {
+                activePromptTitle: 'Choose abomination',
+                cardCondition: { 
+                    location: 'hand', 
+                    controller: 'current', 
+                    condition: card => card.hasKeyword('abomination') 
+                },
+                cardType: ['dude']
+            },
+            handler: context => {
+                this.game.resolveStandardAbility(StandardActions.putIntoPlay({
+                    playType: 'ability',
+                    abilitySourceType: 'card',
+                    reduceAmount: 2,
+                    target: this.gamelocation
+                }, () => {
+                    this.game.addMessage('{0} uses {1} to put {2} into play', context.player, this, context.target);
+                }), context.player, context.target);  
+            }
+        });
+    }
+}
+
+HeFang.code = '11005';
+
+module.exports = HeFang;
---
+++ server/game/cards/09.3-TSs/NicholasKramer.js
@@ -0,0 +1,20 @@
+const DudeCard = require('../../dudecard.js');
+
+class NicholasKramer extends DudeCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => true,
+            match: this,
+            effect: ability.effects.dynamicBullets(() => this.getKramerBulletBonus())
+        });
+    }
+
+    getKramerBulletBonus() {
+        const numOfGadgets = this.controller.cardsInPlay(card => card.isGadget()).length;
+        return numOfGadgets > 4 ? 4 : numOfGadgets;
+    }
+}
+
+NicholasKramer.code = '17007';
+
+module.exports = NicholasKramer;
