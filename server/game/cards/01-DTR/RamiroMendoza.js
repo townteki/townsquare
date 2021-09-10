84bbe6bad54ccddb6954798145c12f37d10c0ba3
AaronSchnabel <aaron.schnabel@gmail.com>
2021-09-10 11:11:26 -0700

    Update RamiroMendoza.js

--- server/game/cards/01-DTR/RamiroMendoza.js
+++ server/game/cards/01-DTR/RamiroMendoza.js
@@ -10,7 +10,7 @@ class RamiroMendoza extends DudeCard {
                 // TODO M2 Ask if player wants to pay
                 if(context.player.getSpendableGhostRock() >= 1) {
                     context.player.spendGhostRock(1);
-                    this.game.addMessage('{0} has to pay 1 GR to {1} to make {1} join the posse', this.controller, this);
+                    this.game.addMessage('{0} has to pay 1 GR to {1} to make him join the posse', this.controller, this);
                 } else {
                     context.player.discardCard(this, false);
                     this.game.addMessage('{0} does not have 1 GR to pay to {1}, who leaves town', this.controller, this);
