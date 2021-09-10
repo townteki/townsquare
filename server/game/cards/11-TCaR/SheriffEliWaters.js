4b4d2d16cc842e0f5ec425069178b08166f84b39
AaronSchnabel <aaron.schnabel@gmail.com>
2021-09-10 11:37:23 -0700

    Update SheriffEliWaters.js

--- server/game/cards/11-TCaR/SheriffEliWaters.js
+++ server/game/cards/11-TCaR/SheriffEliWaters.js
@@ -30,7 +30,7 @@ class SheriffEliWaters extends DudeCard {
                                             title: `Do you want ${this.title} to join posse?`,
                                             onYes: player => {
                                                 this.game.resolveGameAction(GameActions.joinPosse({ card: this }), context).thenExecute(() => {
-                                                    this.game.addMessage('{0} moves {1} to {2}\'s location and joins the shootout as a result of {1}\'s trait', player, this, card);
+                                                    this.game.addMessage('{0} moves {1} to {2}\'s location and joins the shootout as a result of his trait', player, this, card);
                                                 });                                                
                                             },
                                             onNo: player => {
