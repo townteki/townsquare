6c815cdc316f2fc0817ee9fd79e5bebf7f4ba30f
Milan M2 <mmeldo@gmail.com>
2021-10-01 18:01:12 +0200

    Card: Farnum's Consortium, Black Hills Guardians

--- server/game/Deck.js
+++ server/game/Deck.js
@@ -23,6 +23,7 @@ class Deck {
                 type_code: 'outfit',
                 gang_code: this.data.outfit.gang_code,
                 title: this.data.outfit.title,
+                keywords: this.data.outfit.keywords,
                 wealth: this.data.outfit.wealth,
                 production: this.data.outfit.production
             };
