3f6a1392ff6f31be1820e71e7b27926c149d1255
Milan M2 <mmeldo@gmail.com>
2021-10-02 11:54:31 +0200

    Update cards in DTR2

---
+++ server/game/cards/16-DTR2/SpeaksWithEarth2.js
@@ -0,0 +1,7 @@
+const SpeaksWithEarth = require('../09.3-TSs/SpeaksWithEarth');
+
+class SpeaksWithEarth2 extends SpeaksWithEarth {}
+
+SpeaksWithEarth2.code = '25049';
+
+module.exports = SpeaksWithEarth2;
