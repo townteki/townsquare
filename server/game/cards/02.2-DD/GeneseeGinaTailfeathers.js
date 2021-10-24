38988aa26625f71ca047ca9e02aa6b02eb98a8c5
PesiTheWizard <rigningin@simnet.is>
2021-10-24 16:37:45 +0000

    Switching to Pony Express method
    As opposed to the Philip Swinford method, this means the discard is a cost. Practically speaking, as soon as you click the card in hand, it is discarded; and you can't not pick one.

--- server/game/cards/02.2-DD/GeneseeGinaTailfeathers.js
+++ server/game/cards/02.2-DD/GeneseeGinaTailfeathers.js
@@ -1,27 +1,17 @@
 const DudeCard = require('../../dudecard.js');
-const GameActions = require('../../GameActions/index.js');
 
 class GeneseeGinaTailfeathers extends DudeCard {
     setupCardAbilities(ability) {
         this.reaction({
             title: 'React: Genesse Tailfeathers',
             grifter: true,
-            cost: [ability.costs.bootSelf()],
-            handler: context => {
-                this.game.promptForSelect(this.controller, {
-                    promptTitle: this.title,
-                    activePromptTitle: 'Select a card to discard',
-                    waitingPromptTitle: 'Waiting for opponent to discard a card',
-                    cardCondition: card => card.location === 'hand' && card.controller === this.controller,
-                    onSelect: (p, card) => {
-                        this.game.resolveGameAction(GameActions.discardCard({ card: card }, context)).thenExecute(() => {
-                            this.game.resolveGameAction(GameActions.drawCards({ player: p, amount: 2 }), context).thenExecute(() => {
-                                this.game.addMessage('{0} discards {1} to draw two cards thanks to {2}', p, card, this);
-                            });
-                        });
-                    }
-                });
-            }
+            cost: [
+                ability.costs.bootSelf(),
+                ability.costs.discardFromHand()
+            ],
+            message: context => this.game.addMessage('{0} boots {1} to discard {2} from hand and draw two cards',
+                context.player, this, context.costs.discardFromHand),
+            handler: context => context.player.drawCardsToHand(2, context)
         });
     }
 }
