5e13c304f1e9f9ccf31b6e8a862285954e2ec917
Milan M2 <mmeldo@gmail.com>
2021-10-30 19:55:09 +0200

    Update Upkeep discard prompt

--- server/game/gamesteps/upkeep/upkeepprompt.js
+++ server/game/gamesteps/upkeep/upkeepprompt.js
@@ -3,19 +3,25 @@ const AllPlayerPrompt = require('../allplayerprompt.js');
 class UpkeepPrompt extends AllPlayerPrompt {
     constructor(game) {
         super(game);
-        this.title = 'Discard Unwanted Cards with Upkeep';
+        this.title = 'Select Dudes to fire' ;
+        this.selectedCards = [];
     }
 
     completionCondition(player) {
         return player.upkeepPaid;
     }
 
-    activePrompt() {
+    activePrompt(player) {
+        let upkeep = player.determineUpkeep(this.selectedCards);
+        let requiredGR = this.getRequiredUpkeep(player, upkeep);
+        let remainingText = requiredGR > 0 ? ` (${requiredGR} GR required)` : '';
         return {
-            menuTitle: this.title,
+            promptTitle: 'Payday',
+            menuTitle: this.title + remainingText,
             buttons: [
                 { arg: 'selected', text: 'Done' }
-            ]
+            ],
+            selectCard: true
         };
     }
 
@@ -23,15 +29,74 @@ class UpkeepPrompt extends AllPlayerPrompt {
         return { menuTitle: 'Waiting for opponent to pay upkeep' };
     }
 
-    onMenuCommand(player) {
-        let upkeep = player.determineUpkeep();
-        let difference = upkeep - player.ghostrock;
-        if(difference > 0) {
-            this.title = 'Discard Cards to pay Upkeep (' + difference + ' remaining)';
-        } else {
-            player.payUpkeep(upkeep);
-            this.game.addMessage('{0} has paid upkeep of {1} GR', player, upkeep);
+    continue() {
+        for(let player of this.game.getPlayers()) {
+            if(!this.completionCondition(player)) {
+                this.highlightSelectableCards(player);
+            }
         }
+
+        return super.continue();
+    }
+
+    onCardClicked(player, card) {
+        if(card.controller !== player || card.location !== 'play area' ||
+            card.getType() !== 'dude' || card.upkeep === 0) {
+            return false;
+        }
+
+        if(!this.selectedCards.includes(card)) {
+            this.selectedCards.push(card);
+        } else {
+            this.selectedCards = this.selectedCards.filter(selectedCard => selectedCard !== card);
+        }
+        player.setSelectedCards(this.selectedCards);
+    }
+
+    highlightSelectableCards(player) {
+        player.selectCard = true;
+        player.setSelectableCards(this.getDudesWithUpkeep(player));
+    }
+
+    onMenuCommand(player) {
+        let upkeep = player.determineUpkeep(this.selectedCards);
+        let difference = this.getRequiredUpkeep(player, upkeep);
+        if(difference > 0) {
+            return false;
+        }
+        if(this.selectedCards.length) {
+            this.discardCards(player, this.selectedCards, () => {
+                this.payUpkeepAndComplete(player, upkeep);
+            });
+        } else {
+            this.payUpkeepAndComplete(player, upkeep);
+        }
+        return true;
+    }
+
+    payUpkeepAndComplete(player, upkeep) {
+        player.payUpkeep(upkeep);
+        this.game.addMessage('{0} has paid upkeep of {1} GR', player, upkeep);
+        this.selectedCards = [];
+        player.clearSelectedCards();
+        player.clearSelectableCards();
+        super.complete(player);
+    }
+
+    discardCards(player, cards, callback) {
+        if(cards.length > 0) {
+            player.discardCards(cards, false, callback, { isUpkeep: true });
+            this.game.addMessage('{0} does not pay upkeep for {1} and discards them', player, cards);
+        }
+    }
+
+    getRequiredUpkeep(player, upkeep) {
+        const difference = upkeep - player.ghostrock;
+        return difference < 0 ? 0 : difference;
+    }
+
+    getDudesWithUpkeep(player) {
+        return player.cardsInPlay.filter(card => card.getType() === 'dude' && card.upkeep);
     }
 }
 
