1d6688b192975cfef16c95b85cedbcc8df7aa44d
PesiTheWizard <rigningin@simnet.is>
2021-10-30 20:27:34 +0000

    acing as a cost
    using ability.costs.ace
    
    Alternative: ability.costs.placeInDeadPileFromHand ?

--- server/game/cards/01-DTR/GomorraParish.js
+++ server/game/cards/01-DTR/GomorraParish.js
@@ -1,24 +1,17 @@
 const DeedCard = require('../../deedcard.js');
-const GameActions = require('../../GameActions/index.js');
 
 class GomorraParish extends DeedCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Gomorra Parish',
             playType: ['noon'],
-            cost: ability.costs.bootSelf(),
-            target: {
-                activePromptTitle: 'Select a card to ace',
-                cardCondition: { location: 'hand', controller: 'current' },
-                gameAction: 'ace'
-            },
+            cost: [
+                ability.costs.bootSelf(),
+                ability.costs.ace(card => card.location === 'hand' && card.controller === this.controller)
+            ],
             message: context => 
-                this.game.addMessage('{0} uses {1} and aces {2} from hand to gain 1 GR', context.player, this, context.target),
-            handler: context => {
-                this.game.resolveGameAction(GameActions.aceCard({ card: context.target }), context).thenExecute(() => {
-                    context.player.modifyGhostRock(1);
-                });
-            }
+                this.game.addMessage('{0} uses {1} and aces {2} from hand to gain 1 GR', context.player, this, context.costs.ace),
+            handler: context => context.player.modifyGhostRock(1)
         });
     }
 }
