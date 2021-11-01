2aa6fa283ca459e2ca55cc8874ad846edca899e0
Milan M2 <mmeldo@gmail.com>
2021-11-01 23:34:13 +0100

    Card: Harlan Stanton, Bartholomew P. Fountain (#1190)

---
+++ server/game/cards/14-HCWM/HarlanStanton.js
@@ -0,0 +1,32 @@
+const DudeCard = require('../../dudecard.js');
+const GameActions = require('../../GameActions/index.js');
+/** @typedef {import('../../AbilityDsl')} AbilityDsl */
+
+class HarlanStanton extends DudeCard {
+    /** @param {AbilityDsl} ability */
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: () => this.locationCard && this.locationCard.hasKeyword('ranch'),
+            match: this,
+            effect: ability.effects.setAsStud()
+        });
+        
+        this.action({
+            title: 'Shootout: Harlan Stanton',
+            playType: ['shootout:join'],
+            ifCondition: () => this.game.shootout &&
+                this.game.shootout.shootoutLocation.locationCard.hasKeyword('ranch'),
+            ifFailMessage: context =>
+                this.game.addMessage('{0} uses {1}, but it does not have any effect because shootout is not at Ranch', 
+                    context.player, this),
+            message: context => this.game.addMessage('{0} uses {1} to join him to posse', context.player, this),
+            handler: context => {
+                this.game.resolveGameAction(GameActions.joinPosse({ card: this }), context);
+            }
+        });
+    }
+}
+
+HarlanStanton.code = '22009';
+
+module.exports = HarlanStanton;
