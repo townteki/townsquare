3532513201eb77a40bc5503caf558fc06404f79b
Milan M2 <mmeldo@gmail.com>
2021-11-02 11:11:23 +0100

    Card: Blue Lightin', Judge Wells Spicer

---
+++ server/game/cards/14-HCWM/BlueLightnin.js
@@ -0,0 +1,56 @@
+const GoodsCard = require('../../goodscard.js');
+/** @typedef {import('../../AbilityDsl')} AbilityDsl */
+
+class BlueLightnin extends GoodsCard {
+    /** @param {AbilityDsl} ability */
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.setAsStud()
+        });
+
+        this.action({
+            title: 'Shootout: Blue Lightin\'',
+            playType: ['shootout'],
+            repeatable: true,
+            cost: ability.costs.boot(card => card.parent === this.parent &&
+                card.hasKeyword('hex')),
+            message: context => this.game.addMessage('{0} uses {1} and boots {2} to give +1', context.player, this),
+            handler: context => {
+                this.abilityContext = context;
+                this.game.promptWithMenu(context.player, this, {
+                    activePrompt: {
+                        menuTitle: `Choose bonus for ${this.parent.title}`,
+                        buttons: [
+                            { text: '+1 Huckster skill', method: 'giveBonus', arg: 'hucksterBonus' },
+                            { text: '+1 bullet', method: 'giveBonus', arg: 'bulletBonus' }
+                        ]
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+
+    giveBonus(player, arg) {
+        if(arg === 'hucksterBonus') {
+            this.applyAbilityEffect(this.abilityContext.ability, ability => ({
+                match: this.parent,
+                effect: ability.effects.modifySkillRating('huckster', 1)
+            }));
+            this.game.addMessage('{0} uses {1} and boots {2} to give {3} +1 Huckster skill rating', 
+                player, this, this.abilityContext.costs.boot, this.parent);
+        } else if(arg === 'bulletBonus') {
+            this.applyAbilityEffect(this.abilityContext.ability, ability => ({
+                match: this.parent,
+                effect: ability.effects.modifyBullets(1)
+            }));
+            this.game.addMessage('{0} uses {1} and boots {2} to give {3} +1 bullets',
+                player, this, this.abilityContext.costs.boot, this.parent);
+        }
+        return true;        
+    }
+}
+
+BlueLightnin.code = '22038';
+
+module.exports = BlueLightnin;
