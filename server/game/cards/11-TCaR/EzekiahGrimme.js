14350d8bd7636c68a08ddcb24edb25af9ee62fd1
Milan M2 <mmeldo@gmail.com>
2021-11-08 00:36:37 +0100

    Fix pulled card handling
    This is for cards:
    - 108 Drunken Masters
    - Ezekiah Grimme

--- server/game/cards/11-TCaR/EzekiahGrimme.js
+++ server/game/cards/11-TCaR/EzekiahGrimme.js
@@ -26,8 +26,7 @@ class EzekiahGrimme extends LegendCard {
             },
             cost: [ability.costs.bootSelf()],
             handler: context => {
-                // set to null to prevent discarding of pulled card in abilityresolver
-                context.event.context.pull = null;
+                context.event.context.pull.doNotHandlePulledCard = true;
                 this.game.promptForYesNo(context.player, {
                     title: 'Do you want to attach pulled Spell?',
                     onYes: player => {
@@ -42,7 +41,8 @@ class EzekiahGrimme extends LegendCard {
                     },
                     onNo: player => {
                         player.handlePulledCard(context.event.pulledCard);
-                    }
+                    },
+                    source: this
                 });
             }
         });
