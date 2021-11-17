48cd924ecb6e454f5bd1864c2c299b14b5e0ef7a
Milan M2 <mmeldo@gmail.com>
2021-11-18 00:19:11 +0100

    Update for multiple moves

--- server/game/cards/06-TLS/TummyTwister.js
+++ server/game/cards/06-TLS/TummyTwister.js
@@ -2,23 +2,45 @@ const ActionCard = require('../../actioncard.js');
 
 class TummyTwister extends ActionCard {
     setupCardAbilities(ability) {
-        this.persistentEffect({
+        this.whileAttached({
             condition: () => this.parent && this.parent.getType() === 'dude',
-            match: this.parent,
             effect: ability.effects.modifyUpkeep(1)
         });
+
         this.traitReaction({
+            location: 'play area',
+            ignoreActionCosts: true,
             when: {
                 onDudeMoved: event => this.parent && this.parent.getType() === 'deed' &&
-                    event.target === this.parent.gamelocation
+                    event.target === this.parent.gamelocation && 
+                    (!event.parentEvent || event.parentEvent.activeChildEvents.find(event => 
+                        event.name === 'onDudeMoved') === event)
             },
             handler: context => {
-                const saveDeed = this.parent;
-                context.player.attach(this, context.event.card, 'ability', () => {
-                    this.game.addMessage('{0} contracts {1} at {2}', context.event.card, this, saveDeed);
-                });
+                this.saveDeed = this.parent;
+                if(context.event.parentEvent) {
+                    const buttons = context.event.parentEvent.activeChildEvents
+                        .filter(event => event.name === 'onDudeMoved' && event.target === this.saveDeed.gamelocation)
+                        .map(event => { 
+                            return { text: event.card.title, method: 'tummyTwisterInfect', arg: event.card.uuid };
+                        });
+                    if(buttons.length === 1) {
+                        this.tummyTwisterInfect(context.player, context.event.card.uuid);
+                    } else {
+                        this.game.promptWithMenu(this.game.getFirstPlayer(), this, {
+                            activePrompt: {
+                                menuTitle: 'What Dude will get Tummy Twister?',
+                                buttons
+                            },
+                            source: this
+                        });
+                    }
+                } else {
+                    this.tummyTwisterInfect(context.player, context.event.card.uuid);
+                }
             }
         });
+
         this.action({
             title: 'Tummy Twister',
             playType: 'noon',
@@ -30,10 +52,19 @@ class TummyTwister extends ActionCard {
             },
             handler: context => {
                 context.player.attach(this, context.target, 'ability', () => {
-                    this.game.addMessage('{0} uses {1}, infecting {2}', context.player, this, context.target);
+                    this.game.addMessage('{0} uses {1} to infect {2}', context.player, this, context.target);
                 });
             }
         });
+    }
+
+    tummyTwisterInfect(player, arg) {
+        const cardToBeInfected = this.game.findCardInPlayByUuid(arg);
+        cardToBeInfected.controller.attach(this, cardToBeInfected, 'ability', () => {
+            this.game.addMessage('{0}\'s dude {1} contracts {2} at {3}', 
+                cardToBeInfected.controller, cardToBeInfected, this, this.saveDeed);
+        });
+        return true;
     }
 }
 
