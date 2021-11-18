9fabba69f28bed28d5240b2050e8fa42a8ca4654
Milan M2 <mmeldo@gmail.com>
2021-11-18 10:47:50 +0100

    Card: Antoine Peterson, Essence of Armitage

--- docs/cardpool-status.md
+++ docs/cardpool-status.md
@@ -598,7 +598,7 @@
 |:heavy_check_mark:   | 15011| Culpability Scientizer
 |:heavy_check_mark:   | 15012| Espuelas
 |                     | 15013| Rites of the Smoking Mirror
-|                     | 15014| Essence of Armitage
+|:heavy_check_mark:   | 15014| Essence of Armitage
 |                     | 15015| Festering Grasp
 |:heavy_check_mark:   | 15016| Silver Pheasant's Bounty
 |:heavy_check_mark:   | 15017| Comin' Up Roses
@@ -618,7 +618,7 @@
 |                     | 16005| The Harvester
 |                     | 16006| Lucy Clover (Exp.1)
 |:heavy_check_mark:   | 16007| Buckin' Billy Ballard
-|                     | 16008| Antoine Peterson
+|:heavy_check_mark:   | 16008| Antoine Peterson
 |                     | 16009| Denise Brancini
 |:heavy_check_mark:   | 16010| Long Strides Ranch
 |:heavy_check_mark:   | 16011| The Gomorra Gazette
---
+++ server/game/cards/09.1-TCR/EssenceOfArmitage.js
@@ -0,0 +1,34 @@
+const GoodsCard = require('../../goodscard.js');
+/** @typedef {import('../../AbilityDsl')} AbilityDsl */
+
+class EssenceOfArmitage extends GoodsCard {
+    /** @param {AbilityDsl} ability */
+    setupCardAbilities(ability) {
+        this.attachmentRestriction({ keyword: 'abomination' });
+
+        this.persistentEffect({
+            targetController: 'any',
+            condition: () => this.parent && this.parent.isParticipating(),
+            match: card => card.isParticipating() && !card.hasKeyword('abomination'),
+            effect: ability.effects.modifyBullets(-1)
+        });
+
+        this.reaction({
+            title: 'React: Essence of Armitage',
+            when: {
+                onDudeSentHome: event => this.isParticipating() && 
+                    event.card.controller !== this.controller && event.params.isFleeing
+            },
+            cost: ability.costs.bootSelf(),
+            message: context => 
+                this.game.addMessage('{0} uses {1} to give {2} +1 permanent CP', context.player, this, this.parent),
+            handler: () => {
+                this.parent.modifyControl(1);
+            }
+        });
+    }
+}
+
+EssenceOfArmitage.code = '15014';
+
+module.exports = EssenceOfArmitage;
