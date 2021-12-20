2632fdbcc457203921b4ce579ca5f58e7ba5c806
Milan M2 <mmeldo@gmail.com>
2021-12-20 10:43:12 +0100

    Card: Yagn's Mechanical Skeleton for DT2

---
+++ server/game/cards/16-DTR2/YagnsMechanicalSkeleton2.js
@@ -0,0 +1,32 @@
+const GoodsCard = require('../../goodscard.js');
+
+class YagnsMechanicalSkeleton2 extends GoodsCard {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: ability.effects.addKeyword('Gadget')
+        });
+
+        this.reaction({
+            title: 'React: Yagn\'s Mechanical Skeleton',
+            triggerBefore: true,
+            when: {
+                onDudeMoved: event => this.parent && event.card === this.parent &&
+                    event.options.context.player !== this.controller,
+                onCardBooted: event => this.parent && event.card === this.parent &&
+                    event.context.player !== this.controller
+            },
+            cost: ability.costs.bootSelf(),
+            handler: context => {
+                context.replaceHandler(event => {
+                    const eventText = event.name === 'onDudeMoved' ? 'move' : 'boot';
+                    this.game.addMessage('{0} uses {1} to cancel {2} action on {3}', 
+                        context.player, this, eventText, this.parent);
+                });
+            }
+        });
+    }
+}
+
+YagnsMechanicalSkeleton2.code = '13015';
+
+module.exports = YagnsMechanicalSkeleton2;
