70bd1c75ef147c22c48ec8242c0078482f47d8b8
Milan M2 <mmeldo@gmail.com>
2022-01-05 23:40:28 +0100

    Update MeetTheNewBoss2.js

--- server/game/cards/16-DT2/MeetTheNewBoss2.js
+++ server/game/cards/16-DT2/MeetTheNewBoss2.js
@@ -6,22 +6,31 @@ class MeetTheNewBoss2 extends ActionCard {
             title: 'Meet The New Boss',
             playType: 'noon',
             target: 'townsquare',
-            message: context => this.game.addMessage('{0} plays {1} to start a job marking Town Square', context.player, this),
+            message: context => 
+                this.game.addMessage('{0} plays {1} to start a job marking Town Square', context.player, this),
             onSuccess: (job, context) => {
                 if(job.leader.location !== 'play area') {
                     return true;
                 }
+                this.lastingEffect(context.ability, ability => ({
+                    until: {
+                        onCardAbilityResolved: event => event.ability === context.ability
+                    },
+                    match: this,
+                    effect: ability.effects.setActionPlacementLocation(null)
+                })); 
                 job.leader.modifyControl(1);
                 this.game.promptForYesNo(context.player, {
-                    title: `Do you want to ace this card to give ${job.leader} +1 influence?`,
+                    title: `Do you want to ace this card to give ${job.leader.title} +1 influence?`,
                     onYes: player => {
                         job.leader.modifyInfluence(1);
-                        context.source.actionPlacementLocation = 'dead pile';
                         this.game.addMessage('{0} aces {1} to give {2} permanent +1 CP and permanent +1 influence', 
                             player, this, job.leader);
+                        player.moveCard(this, 'dead pile');
                     },
                     onNo: player => {
                         this.game.addMessage('{0} uses {1} to give {2} permanent +1 CP', player, this, job.leader);
+                        player.moveCard(this, 'discard pile');
                     },
                     source: this
                 });
