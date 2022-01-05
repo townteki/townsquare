ffededf1da187abe42fa65911b6c07b3513dbf3b
Milan M2 <mmeldo@gmail.com>
2022-01-06 00:24:15 +0100

    Update WWE: Kidnappin'
    Fixes code for Belligerence

--- server/game/cards/16-DT2/Kidnappin2.js
+++ server/game/cards/16-DT2/Kidnappin2.js
@@ -1,6 +1,9 @@
 const ActionCard = require('../../actioncard.js');
+const GameActions = require('../../GameActions/index.js');
+/** @typedef {import('../../AbilityDsl')} AbilityDsl */
 
 class Kidnappin2 extends ActionCard {
+    /** @param {AbilityDsl} ability */
     setupCardAbilities(ability) {
         this.job({
             title: 'Kidnappin\'',
@@ -11,14 +14,21 @@ class Kidnappin2 extends ActionCard {
                 cardCondition: { location: 'play area', controller: 'opponent' },
                 cardType: 'dude'
             },
+            posseCondition: (job, posseSelection) =>
+                job.leader.bullets + posseSelection.reduce((agg, dude) => agg + dude.bullets, 0) > job.mark.bullets,
             message: context =>
                 this.game.addMessage('{0} plays {1} on {2}', context.player, this, context.target),
-            handler: () => {
-                this.game.once('onLeaderPosseFormed', event => event.shootout.actOnLeaderPosse(dude => dude.increaseBounty()));
+            handler: context => {
+                const eventHandler = event => 
+                    this.game.resolveGameAction(GameActions.addBounty({ card: event.card }), context);
+                this.game.on('onDudeJoinedPosse', eventHandler);
+                this.game.once('onShootoutPhaseFinished', () => {
+                    this.game.removeListener('onDudeJoinedPosse', eventHandler);
+                });
             },
             onSuccess: (job, context) => {
                 if(this.game.discardFromPlay([job.mark], true, () => true, { isCardEffect: true }, context)) {
-                    this.game.addMessage('{0} discards {1} as a result of the {1}', context.player, job.mark, this);
+                    this.game.addMessage('{0} discards {1} as a result of the {2}', context.player, job.mark, this);
                 }
             }
         });
