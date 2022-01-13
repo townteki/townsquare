a50d3c955ffac00dbd34ebf5c80195843db7dc4d
Steve Holder <steve.holder@gmail.com>
2022-01-12 18:56:18 -0800

    fixes per review comments

--- server/game/cards/10-BMR/TheLawGoesUnderground.js
+++ server/game/cards/10-BMR/TheLawGoesUnderground.js
@@ -15,18 +15,18 @@ class TheLawGoesUnderground extends ActionCard {
                     condition: card => this.game.shootout && card.isStud() && card.isParticipating()
                 }),
                 'Discard Stud': ability.costs.discardFromPlay({
+                    controller: 'current',
                     type: 'dude',
-                    condition: card => this.game.shootout && card.isStud() && this.game.shootout.getPosseByPlayer(this.controller).isInPosse(card)
+                    condition: card => this.game.shootout && card.isStud() && card.isParticipating()
                 })
             }),
-            message: context => this.game.addMessage('{0} uses {1} to flee to their hideout', context.player, this),
             handler: context => {
                 const actor = context.costs.boot ? context.costs.boot : context.costs.discardFromPlay;
                 const actorIsDeputy = actor.hasKeyword('deputy');
                 if(actorIsDeputy) {
                     const opposingShooter = this.game.shootout.getPosseByPlayer(context.player.getOpponent()).shooter;
                     this.game.resolveGameAction(GameActions.sendHome({ card: opposingShooter, options: { needToBoot: true } }), context);
-                    this.game.addMessage('{0} sends {1} home because {2} is a Deputy', context.player, opposingShooter, actor);
+                    this.game.addMessage('{0} uses {1} to send {2} home because {3} is a Deputy', context.player, this, opposingShooter, actor);
                 }
                 this.game.shootout.actOnPlayerPosse(context.player, card => this.game.shootout.sendHome(card, context));
                 this.game.addMessage('{0} uses {1} and {2} {3} to flee to their hideout', context.player, this, context.costs.boot ? 'boots' : 'discards', actor);
