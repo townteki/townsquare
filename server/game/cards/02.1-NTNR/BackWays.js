afcd3f3af9f7724109b56222715ad2c7586d3ae7
PesiTheWizard <rigningin@simnet.is>
2022-02-20 09:06:05 +0000

    Back Ways
    * Adding file for Back Ways
    
    * Marking Back Ways

---
+++ server/game/cards/02.1-NTNR/BackWays.js
@@ -0,0 +1,44 @@
+const ActionCard = require('../../actioncard.js');
+const GameActions = require('../../GameActions/index.js');
+
+class BackWays extends ActionCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Back Ways',
+            playType: ['noon'],
+            target: {
+                activePromptTitle: 'Select your wanted dude to move',
+                cardCondition: {
+                    location: 'play area',
+                    controller: 'current',
+                    wanted: true
+                },
+                cardType: ['dude'],
+                gameAction: 'moveDude'
+            },
+            handler: context => {
+                const erstwhileLocation = context.target.locationCard;
+                this.game.promptForLocation(context.player, {
+                    activePromptTitle: 'Choose destination for ' + context.target.title,
+                    cardCondition: {
+                        location: 'play area',
+                        condition: card => card !== erstwhileLocation
+                    },
+                    onSelect: (player, location) => {
+                        this.game.resolveGameAction(GameActions.moveDude({ 
+                            card: context.target, 
+                            targetUuid: location.uuid
+                        }), context);
+                        this.game.addMessage('{0} uses {1} to move {2} from {3} to {4}',
+                            player, this, context.target, erstwhileLocation, location);
+                    },
+                    source: this
+                });
+            }
+        });
+    }
+}
+
+BackWays.code = '02019';
+
+module.exports = BackWays;
