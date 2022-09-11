6ae15d6aa4f20d147fffa147e8a94e4c47fa9589
AaronSchnabel <aaron.schnabel@gmail.com>
2022-09-11 06:04:22 -0700

    Fix Disarm so that player doesn't take casualties (#1443)

--- server/game/cards/11-TCaR/Disarm.js
+++ server/game/cards/11-TCaR/Disarm.js
@@ -12,55 +12,64 @@ class Disarm extends ActionCard {
                 cardType: ['dude']
             },
             handler: context => {
-                context.ability.selectAnotherTarget(context.player, context, {
-                    activePromptTitle: 'Select attachment',
-                    waitingPromptTitle: 'Waiting for opponent to select attachment',
-                    cardCondition: card => card.parent === context.target,
-                    onSelect: (player, att) => {
-                        if(this.game.shootout) {
-                            if(att.isUnique()) {
-                                this.game.resolveGameAction(GameActions.discardCard({ card: att }), context).thenExecute(() => {
-                                    this.game.addMessage('{0} uses {1} to discard {2}\'s attachment {3} and to ignore all casualties', 
+                if(context.target.hasAttachment()) {
+                    context.ability.selectAnotherTarget(context.player, context, {
+                        activePromptTitle: 'Select attachment',
+                        waitingPromptTitle: 'Waiting for opponent to select attachment',
+                        cardCondition: card => card.parent === context.target,
+                        onSelect: (player, att) => {
+                            if(this.game.shootout) {
+                                if(att.isUnique()) {
+                                    this.game.resolveGameAction(GameActions.discardCard({ card: att }), context).thenExecute(() => {
+                                        this.game.addMessage('{0} uses {1} to discard {2}\'s attachment {3}', 
+                                            context.player, this, context.target, att);
+                                    });
+                                } else {
+                                    this.game.resolveGameAction(GameActions.aceCard({ card: att }), context).thenExecute(() => {
+                                        this.game.addMessage('{0} uses {1} to ace {2}\'s attachment {3}', 
+                                            context.player, this, context.target, att);
+                                    });                                
+                                }
+                                this.game.resolveGameAction(GameActions.decreaseCasualties({ player: context.player }), context);
+                            } else {
+                                this.game.resolveGameAction(GameActions.bootCard({ card: att }), context).thenExecute(() => {
+                                    this.game.addMessage('{0} uses {1} to boot {2}\'s attachment {3}', 
                                         context.player, this, context.target, att);
                                 });
-                            } else {
-                                this.game.resolveGameAction(GameActions.aceCard({ card: att }), context).thenExecute(() => {
-                                    this.game.addMessage('{0} uses {1} to ace {2}\'s attachment {3} and to ignore all casualties', 
-                                        context.player, this, context.target, att);
-                                });                                
                             }
-                        } else {
-                            this.game.resolveGameAction(GameActions.bootCard({ card: att }), context).thenExecute(() => {
-                                this.game.addMessage('{0} uses {1} to boot {2}\'s attachment {3} and to ignore all casualties', 
-                                    context.player, this, context.target, att);
-                            });
-                        }
-                        this.game.resolveGameAction(GameActions.decreaseCasualties({ player: context.player }), context); 
-                        if(!context.player.isCheatin()) {
-                            context.ability.selectAnotherTarget(context.player, context, {
-                                activePromptTitle: 'Select your dude',
-                                waitingPromptTitle: 'Waiting for opponent to select dude',
-                                cardCondition: card => card.location === 'play area' &&
-                                    card.controller === context.player,
-                                cardType: 'dude',
-                                onSelect: (player, dude) => {
-                                    this.untilEndOfRound(context.ability, ability => ({
-                                        match: dude,
-                                        effect: [
-                                            ability.effects.modifyBullets(2),
-                                            ability.effects.setAsStud()
-                                        ]
-                                    }));
-                                    this.game.addMessage('{0} uses {1} to give {2} +2 bullets and make them a stud', 
-                                        context.player, this, dude);
-                                    return true;
-                                },
-                                source: this
-                            });
-                        }
-                        return true;
-                    },
-                    source: this
+                            return true;
+                        },
+                        source: this
+                    });
+                }
+                this.game.queueSimpleStep(() => { 
+                    if(this.game.shootout) {
+                        this.game.resolveGameAction(GameActions.decreaseCasualties({ player: context.player }), context).thenExecute(() => {
+                            this.game.addMessage('{0} uses {1} to ignore all casualties this round', context.player, this);
+                        });
+                    }
+                    if(!context.player.isCheatin()) {
+                        context.ability.selectAnotherTarget(context.player, context, {
+                            activePromptTitle: 'Select your dude',
+                            waitingPromptTitle: 'Waiting for opponent to select dude',
+                            cardCondition: card => card.location === 'play area' &&
+                                card.controller === context.player,
+                            cardType: 'dude',
+                            onSelect: (player, dude) => {
+                                this.untilEndOfRound(context.ability, ability => ({
+                                    match: dude,
+                                    effect: [
+                                        ability.effects.modifyBullets(2),
+                                        ability.effects.setAsStud()
+                                    ]
+                                }));
+                                this.game.addMessage('{0} uses {1} to give {2} +2 bullets and make them a stud', 
+                                    context.player, this, dude);
+                                return true;
+                            },
+                            source: this
+                        });
+                    }
                 });
             }
         });
