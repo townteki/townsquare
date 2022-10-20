7d229bbc7f19474ebea22019036114d0e437e953
Milan M2 <mmeldo@gmail.com>
2022-10-20 21:23:50 +0200

    Update JasperStone.js

--- server/game/cards/11-TCaR/JasperStone.js
+++ server/game/cards/11-TCaR/JasperStone.js
@@ -3,7 +3,8 @@ const LegendCard = require('../../legendcard.js');
 class JasperStone extends LegendCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => this.game.shootout && !this.game.shootout.shootoutLocation.isHome(this.owner),
+            condition: () => this.game.shootout && this.game.shootout.shootoutLocation && 
+                !this.game.shootout.shootoutLocation.isHome(this.owner),
             match: this.owner,
             effect: ability.effects.onlyShooterContributes()
         });
