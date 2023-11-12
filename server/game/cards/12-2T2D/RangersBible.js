c4f4e228e2ba2c2f33312a129ba116f490f4bb4d
Milan M2 <mmeldo@gmail.com>
2023-11-12 23:17:39 +0100

    Ranger's Bible can be used only in shootout

--- server/game/cards/12-2T2D/RangersBible.js
+++ server/game/cards/12-2T2D/RangersBible.js
@@ -9,10 +9,16 @@ class RangersBible extends GoodsCard {
             playType: ['cheatin resolution'],
             cost: ability.costs.bootSelf(),
             choosePlayer: true,
-            ifCondition: context => !context.player.isCheatin(),
-            ifFailMessage: context =>
-                this.game.addMessage('{0} uses {1}, but it does not have any effect because {0} is cheatin\'', 
-                    context.player, this),
+            ifCondition: context => this.game.shootout && !context.player.isCheatin(),
+            ifFailMessage: context => {
+                if(!this.game.shootout) {
+                    this.game.addMessage('{0} uses {1}, but it does not have any effect because it is not shootout', 
+                        context.player, this);
+                } else {
+                    this.game.addMessage('{0} uses {1}, but it does not have any effect because {0} is cheatin\'', 
+                        context.player, this);
+                }
+            },
             handler: context => {
                 const parentInf = this.parent.influence > 4 ? 4 : this.parent.influence;
                 if(context.chosenPlayer.modifyRank(-1 * parentInf, context)) {
